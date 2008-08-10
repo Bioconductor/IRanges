0e4885c8a40134ca5253a33ce55f0332736de8af
Herve Pages <hpages@fhcrc.org>
2008-08-10 02:17:43 +0000

    [code cleaning] Use of macros in C code in charge of .Call entry points and C callable registration. This makes the code cleaner, shorter, and easier to read/maintain.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@33151 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_init_IRanges.c
+++ src/R_init_IRanges.c
@@ -1,22 +1,25 @@
 #include "IRanges.h"
 
+#define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
+#define REGISTER_CCALLABLE(fun) R_RegisterCCallable("IRanges", #fun, (DL_FUNC) &fun)
+
 static const R_CallMethodDef callMethods[] = {
 
 /* AEbufs.c */
-	{"debug_AEbufs", (DL_FUNC) &debug_AEbufs, 0},
+	CALLMETHOD_DEF(debug_AEbufs, 0),
 
 /* IRanges_class.c */
-	{"debug_IRanges_class", (DL_FUNC) &debug_IRanges_class, 0},
+	CALLMETHOD_DEF(debug_IRanges_class, 0),
 
 /* IRanges_utils.c */
-	{"debug_IRanges_utils", (DL_FUNC) &debug_IRanges_utils, 0},
+	CALLMETHOD_DEF(debug_IRanges_utils, 0),
 
-	{"narrow_IRanges", (DL_FUNC) &narrow_IRanges, 4},
-	{"int_to_adjacent_ranges", (DL_FUNC) &int_to_adjacent_ranges, 1},
-	{"which_as_ranges", (DL_FUNC) &which_as_ranges, 1},
-	{"reduce_IRanges", (DL_FUNC) &reduce_IRanges, 2},
-	{"IRanges_coverage", (DL_FUNC) &IRanges_coverage, 3},
-	{"summary_IRanges_list", (DL_FUNC) &summary_IRanges_list, 1},
+	CALLMETHOD_DEF(narrow_IRanges, 4),
+	CALLMETHOD_DEF(int_to_adjacent_ranges, 1),
+	CALLMETHOD_DEF(which_as_ranges, 1),
+	CALLMETHOD_DEF(reduce_IRanges, 2),
+	CALLMETHOD_DEF(IRanges_coverage, 3),
+	CALLMETHOD_DEF(summary_IRanges_list, 1),
 
 	{NULL, NULL, 0}
 };
@@ -27,43 +30,43 @@ void R_init_IRanges(DllInfo *info)
 	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 
 /* AEbufs.c */
-	R_RegisterCCallable("IRanges", "_IntAE_set_val", (DL_FUNC) &_IntAE_set_val);
-	R_RegisterCCallable("IRanges", "_new_IntAE", (DL_FUNC) &_new_IntAE);
-	R_RegisterCCallable("IRanges", "_IntAE_insert_at", (DL_FUNC) &_IntAE_insert_at);
-	R_RegisterCCallable("IRanges", "_IntAE_append", (DL_FUNC) &_IntAE_append);
-	R_RegisterCCallable("IRanges", "_IntAE_delete_at", (DL_FUNC) &_IntAE_delete_at);
-	R_RegisterCCallable("IRanges", "_IntAE_sum_val", (DL_FUNC) &_IntAE_sum_val);
-	R_RegisterCCallable("IRanges", "_IntAE_append_shifted_vals", (DL_FUNC) &_IntAE_append_shifted_vals);
-	R_RegisterCCallable("IRanges", "_IntAE_sum_IntAE", (DL_FUNC) &_IntAE_sum_IntAE);
-	R_RegisterCCallable("IRanges", "_IntAE_qsort", (DL_FUNC) &_IntAE_qsort);
-	R_RegisterCCallable("IRanges", "_IntAE_delete_adjdups", (DL_FUNC) &_IntAE_delete_adjdups);
-	R_RegisterCCallable("IRanges", "_IntAE_asINTEGER", (DL_FUNC) &_IntAE_asINTEGER);
-	R_RegisterCCallable("IRanges", "_INTEGER_asIntAE", (DL_FUNC) &_INTEGER_asIntAE);
-	R_RegisterCCallable("IRanges", "_CHARACTER_asIntAE", (DL_FUNC) &_CHARACTER_asIntAE);
-	R_RegisterCCallable("IRanges", "_new_IntAEAE", (DL_FUNC) &_new_IntAEAE);
-	R_RegisterCCallable("IRanges", "_IntAEAE_insert_at", (DL_FUNC) &_IntAEAE_insert_at);
-	R_RegisterCCallable("IRanges", "_IntAEAE_eltwise_append", (DL_FUNC) &_IntAEAE_eltwise_append);
-	R_RegisterCCallable("IRanges", "_IntAEAE_sum_val", (DL_FUNC) &_IntAEAE_sum_val);
-	R_RegisterCCallable("IRanges", "_IntAEAE_asLIST", (DL_FUNC) &_IntAEAE_asLIST);
-	R_RegisterCCallable("IRanges", "_LIST_asIntAEAE", (DL_FUNC) &_LIST_asIntAEAE);
-	R_RegisterCCallable("IRanges", "_IntAEAE_toEnvir", (DL_FUNC) &_IntAEAE_toEnvir);
-	R_RegisterCCallable("IRanges", "_new_RangeAE", (DL_FUNC) &_new_RangeAE);
-	R_RegisterCCallable("IRanges", "_RangeAE_insert_at", (DL_FUNC) &_RangeAE_insert_at);
-	R_RegisterCCallable("IRanges", "_new_CharAE", (DL_FUNC) &_new_CharAE);
-	R_RegisterCCallable("IRanges", "_new_CharAE_from_string", (DL_FUNC) &_new_CharAE_from_string);
-	R_RegisterCCallable("IRanges", "_CharAE_insert_at", (DL_FUNC) &_CharAE_insert_at);
-	R_RegisterCCallable("IRanges", "_CharAE_asRAW", (DL_FUNC) &_CharAE_asRAW);
-	R_RegisterCCallable("IRanges", "_new_CharAEAE", (DL_FUNC) &_new_CharAEAE);
-	R_RegisterCCallable("IRanges", "_CharAEAE_insert_at", (DL_FUNC) &_CharAEAE_insert_at);
-	R_RegisterCCallable("IRanges", "_append_string_to_CharAEAE", (DL_FUNC) &_append_string_to_CharAEAE);
+	REGISTER_CCALLABLE(_IntAE_set_val);
+	REGISTER_CCALLABLE(_new_IntAE);
+	REGISTER_CCALLABLE(_IntAE_insert_at);
+	REGISTER_CCALLABLE(_IntAE_append);
+	REGISTER_CCALLABLE(_IntAE_delete_at);
+	REGISTER_CCALLABLE(_IntAE_sum_val);
+	REGISTER_CCALLABLE(_IntAE_append_shifted_vals);
+	REGISTER_CCALLABLE(_IntAE_sum_IntAE);
+	REGISTER_CCALLABLE(_IntAE_qsort);
+	REGISTER_CCALLABLE(_IntAE_delete_adjdups);
+	REGISTER_CCALLABLE(_IntAE_asINTEGER);
+	REGISTER_CCALLABLE(_INTEGER_asIntAE);
+	REGISTER_CCALLABLE(_CHARACTER_asIntAE);
+	REGISTER_CCALLABLE(_new_IntAEAE);
+	REGISTER_CCALLABLE(_IntAEAE_insert_at);
+	REGISTER_CCALLABLE(_IntAEAE_eltwise_append);
+	REGISTER_CCALLABLE(_IntAEAE_sum_val);
+	REGISTER_CCALLABLE(_IntAEAE_asLIST);
+	REGISTER_CCALLABLE(_LIST_asIntAEAE);
+	REGISTER_CCALLABLE(_IntAEAE_toEnvir);
+	REGISTER_CCALLABLE(_new_RangeAE);
+	REGISTER_CCALLABLE(_RangeAE_insert_at);
+	REGISTER_CCALLABLE(_new_CharAE);
+	REGISTER_CCALLABLE(_new_CharAE_from_string);
+	REGISTER_CCALLABLE(_CharAE_insert_at);
+	REGISTER_CCALLABLE(_CharAE_asRAW);
+	REGISTER_CCALLABLE(_new_CharAEAE);
+	REGISTER_CCALLABLE(_CharAEAE_insert_at);
+	REGISTER_CCALLABLE(_append_string_to_CharAEAE);
 
 /* IRanges_class.c */
-	R_RegisterCCallable("IRanges", "_get_IRanges_start", (DL_FUNC) &_get_IRanges_start);
-	R_RegisterCCallable("IRanges", "_get_IRanges_width", (DL_FUNC) &_get_IRanges_width);
-	R_RegisterCCallable("IRanges", "_get_IRanges_length", (DL_FUNC) &_get_IRanges_length);
-	R_RegisterCCallable("IRanges", "_set_IRanges_names", (DL_FUNC) &_set_IRanges_names);
-	R_RegisterCCallable("IRanges", "_copy_IRanges_slots", (DL_FUNC) &_copy_IRanges_slots);
-	R_RegisterCCallable("IRanges", "_new_IRanges", (DL_FUNC) &_new_IRanges);
-	R_RegisterCCallable("IRanges", "_alloc_IRanges", (DL_FUNC) &_alloc_IRanges);
+	REGISTER_CCALLABLE(_get_IRanges_start);
+	REGISTER_CCALLABLE(_get_IRanges_width);
+	REGISTER_CCALLABLE(_get_IRanges_length);
+	REGISTER_CCALLABLE(_set_IRanges_names);
+	REGISTER_CCALLABLE(_copy_IRanges_slots);
+	REGISTER_CCALLABLE(_new_IRanges);
+	REGISTER_CCALLABLE(_alloc_IRanges);
 }
 
