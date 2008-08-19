6b47c7f45cf72ab47e6394b3282f7e2098488120
Patrick Aboyoun <unknown>
2008-08-19 22:12:29 +0000

    Moved the reverse generic definition and the IRanges and MaskCollection methods to the IRanges package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@33281 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/reverse.R
@@ -0,0 +1,50 @@
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### The "reverse" generic and methods.
+###
+
+setGeneric("reverse", signature="x",
+    function(x, ...) standardGeneric("reverse")
+)
+
+### This method does NOT preserve normality.
+.IRanges.reverse <- function(x, ...)
+{
+    args <- extraArgsAsList(NULL, ...)
+    argnames <- names(args)
+    n2p <- match(c("start", "end", "use.names"), argnames)
+    if (is.na(n2p[1]))
+        stop("'start' must be specified for \"reverse\" method for IRanges objects")
+    start <- normargSingleStart(args[[n2p[1]]])
+    if (is.na(n2p[2]))
+        stop("'end' must be specified for \"reverse\" method for IRanges objects")
+    end <- normargSingleEnd(args[[n2p[2]]])
+    if (!is.na(n2p[3]) && !normargUseNames(args[[n2p[3]]])) {
+        unsafe.names(x) <- NULL
+    }
+    unsafe.start(x) <- start + end - end(x)
+    x
+}
+
+setMethod("reverse", "IRanges", .IRanges.reverse)
+
+setMethod("reverse", "NormalIRanges",
+    function(x, ...)
+    {
+        ## callNextMethod() temporarily breaks 'x' as a NormalIRanges object
+        ## because the returned ranges are ordered from right to left.
+        x <- callNextMethod()
+        unsafe.update(x, start=rev(start(x)), width=rev(width(x)), names=rev(names(x)))
+    }
+)
+
+setMethod("reverse", "MaskCollection",
+    function(x, ...)
+    {
+        start <- 1L
+        end <- width(x)
+        x@nir_list <- lapply(nir_list(x),
+            function(nir) reverse(nir, start=start, end=end)
+        )
+        x
+    }
+)
