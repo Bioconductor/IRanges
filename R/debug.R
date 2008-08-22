1b3a44ab1f087b4389ef81353f5e55c9357581be
Patrick Aboyoun <unknown>
2008-08-22 23:22:57 +0000

    Added a function to the debug suite.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@33344 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/debug.R
+++ R/debug.R
@@ -3,11 +3,14 @@
 debug_AEbufs <- function()
     invisible(.Call("debug_AEbufs", PACKAGE="IRanges"))
 
+debug_X_utils <- function()
+    invisible(.Call("debug_X_utils", PACKAGE="IRanges"))
+
 debug_XInteger <- function()
-	invisible(.Call("debug_XInteger", PACKAGE="IRanges"))
+    invisible(.Call("debug_XInteger", PACKAGE="IRanges"))
 
 debug_XNumeric <- function()
-	invisible(.Call("debug_XNumeric", PACKAGE="IRanges"))
+    invisible(.Call("debug_XNumeric", PACKAGE="IRanges"))
 
 debug_IRanges_class <- function()
     invisible(.Call("debug_IRanges_class", PACKAGE="IRanges"))
