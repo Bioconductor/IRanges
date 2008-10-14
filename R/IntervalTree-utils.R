98bee6ca2c70e492502b97bf88a8a02a88c27e22
Michael Lawrence <lawremi@iastate.edu>
2008-10-14 14:59:33 +0000

    Add 'by' argument to overlap() to give a minimum amount of overlap.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@34467 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/IntervalTree-utils.R
+++ R/IntervalTree-utils.R
@@ -3,11 +3,16 @@
 ### -------------------------------------------------------------------------
 
 setMethod("overlap", c("IntervalTree", "IRanges"),
-          function(object, query, multiple = TRUE)
+          function(object, query, by = 1, multiple = TRUE)
           {
             validObject(query)
             if (!isTRUEorFALSE(multiple))
               stop("'multiple' must be logical of length 1")
+            if (!isSingleNumber(by))
+              stop("'by' must be a single number")
+            if (!missing(by)) {
+              width(query) <- width(query) - (by - 1) * 2
+            }
             fun <- "overlap"
             if (multiple)
               fun <- paste(fun, "_multiple", sep = "")
