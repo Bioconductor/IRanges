a15db14bf6258a2b42f3d6d4f94c65e11d63f5a7
Herve Pages <hpages@fhcrc.org>
2008-12-10 00:42:11 +0000

    #Removed temporary "redirection" of the XRaw class to the RawPtr class (now an XRaw object is what it is supposed to be i.e. an XSequence object with a RawPtr xdata slot).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@35800 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/XRaw-class.R
+++ R/XRaw-class.R
@@ -6,16 +6,10 @@
 ### bytes (stored as char values at the C level).
 ###
 
-#setClass("XRaw",
-#    contains="XSequence",
-#    representation(
-#        xdata="RawPtr"
-#    )
-#)
+setClass("XRaw",
+    contains="XSequence",
+    representation(
+        xdata="RawPtr"
+    )
+)
 
-### A temporary "redirection" of the XRaw class to the RawPtr class. Only
-### during the time needed to migrate all the BSgenome data packages to the
-### new RawPtr class.
-### TODO: Implement the real XRaw class once all the BSgenome data packages
-### are fixed.
-setClass("XRaw", contains="RawPtr")
