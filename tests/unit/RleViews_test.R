abd539abc9e0b2160c6db5a3a8a24ef3b3e4395e
Patrick Aboyoun <unknown>
2009-01-02 07:05:25 +0000

    Changed the checkIdentical to checkEquals in views calculations.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@36401 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/unit/RleViews_test.R
+++ tests/unit/RleViews_test.R
@@ -5,12 +5,12 @@ test_RleViews <- function() {
     xList <-
       lapply(seq_len(length(xRleViews)),
              function(i) subseq(x, start = start(xRleViews)[i], end = end(xRleViews)[i]))
-    checkIdentical(sapply(xList[-1], min), viewMins(xRleViews[-1]))
-    checkIdentical(sapply(xList[-1], min, na.rm = TRUE), viewMins(xRleViews[-1], na.rm = TRUE))
-    checkIdentical(sapply(xList[-1], max), viewMaxs(xRleViews[-1]))
-    checkIdentical(sapply(xList[-1], max, na.rm = TRUE), viewMaxs(xRleViews[-1], na.rm = TRUE))
-    checkIdentical(sapply(xList, sum), viewSums(xRleViews))
-    checkIdentical(sapply(xList, sum, na.rm = TRUE), viewSums(xRleViews, na.rm = TRUE))
+    checkEquals(sapply(xList[-1], min), viewMins(xRleViews[-1]))
+    checkEquals(sapply(xList[-1], min, na.rm = TRUE), viewMins(xRleViews[-1], na.rm = TRUE))
+    checkEquals(sapply(xList[-1], max), viewMaxs(xRleViews[-1]))
+    checkEquals(sapply(xList[-1], max, na.rm = TRUE), viewMaxs(xRleViews[-1], na.rm = TRUE))
+    checkEquals(sapply(xList, sum), viewSums(xRleViews))
+    checkEquals(sapply(xList, sum, na.rm = TRUE), viewSums(xRleViews, na.rm = TRUE))
 
     y <- rep(c(NA, 3.4, NA, 7.8, 9.0), 1:5)
     yRle <- Rle(y)
@@ -18,12 +18,12 @@ test_RleViews <- function() {
     yList <-
       lapply(seq_len(length(yRleViews)),
              function(i) subseq(y, start = start(yRleViews)[i], end = end(yRleViews)[i]))
-    checkIdentical(sapply(yList, min), viewMins(yRleViews))
-    suppressWarnings(checkIdentical(sapply(yList, min, na.rm = TRUE), viewMins(yRleViews, na.rm = TRUE)))
-    checkIdentical(sapply(yList, max), viewMaxs(yRleViews))
-    suppressWarnings(checkIdentical(sapply(yList, max, na.rm = TRUE), viewMaxs(yRleViews, na.rm = TRUE)))
-    checkIdentical(sapply(yList, sum), viewSums(yRleViews))
-    checkIdentical(sapply(yList, sum, na.rm = TRUE), viewSums(yRleViews, na.rm = TRUE))
+    checkEquals(sapply(yList, min), viewMins(yRleViews))
+    suppressWarnings(checkEquals(sapply(yList, min, na.rm = TRUE), viewMins(yRleViews, na.rm = TRUE)))
+    checkEquals(sapply(yList, max), viewMaxs(yRleViews))
+    suppressWarnings(checkEquals(sapply(yList, max, na.rm = TRUE), viewMaxs(yRleViews, na.rm = TRUE)))
+    checkEquals(sapply(yList, sum), viewSums(yRleViews))
+    checkEquals(sapply(yList, sum, na.rm = TRUE), viewSums(yRleViews, na.rm = TRUE))
 
     z <- rep(c(NA, 3.4-1i, NA, 7.8+3i, 9.0-2i), 1:5)
     zRle <- Rle(z)
@@ -31,6 +31,6 @@ test_RleViews <- function() {
     zList <-
       lapply(seq_len(length(zRleViews)),
              function(i) subseq(z, start = start(zRleViews)[i], end = end(zRleViews)[i]))
-    checkIdentical(sapply(zList, sum), viewSums(zRleViews))
-    checkIdentical(sapply(zList, sum, na.rm = TRUE), viewSums(zRleViews, na.rm = TRUE))
+    checkEquals(sapply(zList, sum), viewSums(zRleViews))
+    checkEquals(sapply(zList, sum, na.rm = TRUE), viewSums(zRleViews, na.rm = TRUE))
 }
