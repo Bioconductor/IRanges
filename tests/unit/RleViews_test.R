1ce1f5a3ee9c3bf3194497e24b5bb92e1912388f
Patrick Aboyoun <unknown>
2009-01-02 23:34:24 +0000

    Changed checkEquals to checkEqualsNumeric.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@36413 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/unit/RleViews_test.R
+++ tests/unit/RleViews_test.R
@@ -5,12 +5,12 @@ test_RleViews <- function() {
     xList <-
       lapply(seq_len(length(xRleViews)),
              function(i) subseq(x, start = start(xRleViews)[i], end = end(xRleViews)[i]))
-    checkEquals(sapply(xList, min), viewMins(xRleViews))
-    checkEquals(sapply(xList, min, na.rm = TRUE), viewMins(xRleViews, na.rm = TRUE))
-    checkEquals(sapply(xList, max), viewMaxs(xRleViews))
-    checkEquals(sapply(xList, max, na.rm = TRUE), viewMaxs(xRleViews, na.rm = TRUE))
-    checkEquals(sapply(xList, sum), viewSums(xRleViews))
-    checkEquals(sapply(xList, sum, na.rm = TRUE), viewSums(xRleViews, na.rm = TRUE))
+    checkEqualsNumeric(sapply(xList, min), viewMins(xRleViews))
+    checkEqualsNumeric(sapply(xList, min, na.rm = TRUE), viewMins(xRleViews, na.rm = TRUE))
+    checkEqualsNumeric(sapply(xList, max), viewMaxs(xRleViews))
+    checkEqualsNumeric(sapply(xList, max, na.rm = TRUE), viewMaxs(xRleViews, na.rm = TRUE))
+    checkEqualsNumeric(sapply(xList, sum), viewSums(xRleViews))
+    checkEqualsNumeric(sapply(xList, sum, na.rm = TRUE), viewSums(xRleViews, na.rm = TRUE))
 
     y <- rep(c(1.2, 3.4, NA, 7.8, 9.0), 1:5)
     yRle <- Rle(y)
@@ -18,12 +18,12 @@ test_RleViews <- function() {
     yList <-
       lapply(seq_len(length(yRleViews)),
              function(i) subseq(y, start = start(yRleViews)[i], end = end(yRleViews)[i]))
-    checkEquals(sapply(yList, min), viewMins(yRleViews))
-    suppressWarnings(checkEquals(sapply(yList, min, na.rm = TRUE), viewMins(yRleViews, na.rm = TRUE)))
-    checkEquals(sapply(yList, max), viewMaxs(yRleViews))
-    suppressWarnings(checkEquals(sapply(yList, max, na.rm = TRUE), viewMaxs(yRleViews, na.rm = TRUE)))
-    checkEquals(sapply(yList, sum), viewSums(yRleViews))
-    checkEquals(sapply(yList, sum, na.rm = TRUE), viewSums(yRleViews, na.rm = TRUE))
+    checkEqualsNumeric(sapply(yList, min), viewMins(yRleViews))
+    suppressWarnings(checkEqualsNumeric(sapply(yList, min, na.rm = TRUE), viewMins(yRleViews, na.rm = TRUE)))
+    checkEqualsNumeric(sapply(yList, max), viewMaxs(yRleViews))
+    suppressWarnings(checkEqualsNumeric(sapply(yList, max, na.rm = TRUE), viewMaxs(yRleViews, na.rm = TRUE)))
+    checkEqualsNumeric(sapply(yList, sum), viewSums(yRleViews))
+    checkEqualsNumeric(sapply(yList, sum, na.rm = TRUE), viewSums(yRleViews, na.rm = TRUE))
 
     z <- rep(c(1+1i, 3.4-1i, NA, 7.8+3i, 9.0-2i), 1:5)
     zRle <- Rle(z)
@@ -31,6 +31,6 @@ test_RleViews <- function() {
     zList <-
       lapply(seq_len(length(zRleViews)),
              function(i) subseq(z, start = start(zRleViews)[i], end = end(zRleViews)[i]))
-    checkEquals(sapply(zList, sum), viewSums(zRleViews))
-    checkEquals(sapply(zList, sum, na.rm = TRUE), viewSums(zRleViews, na.rm = TRUE))
+    checkEqualsNumeric(sapply(zList, sum), viewSums(zRleViews))
+    checkEqualsNumeric(sapply(zList, sum, na.rm = TRUE), viewSums(zRleViews, na.rm = TRUE))
 }
