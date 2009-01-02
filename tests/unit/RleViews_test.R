19b3fbaa27d9e4f1f2ab66cab999c649053342ac
Patrick Aboyoun <unknown>
2009-01-02 21:37:09 +0000

    Updated tests for RleViews objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@36409 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/unit/RleViews_test.R
+++ tests/unit/RleViews_test.R
@@ -1,18 +1,18 @@
 test_RleViews <- function() {
-    x <- rep(c(NA, 3L, NA, 7L, 9L), 1:5)
+    x <- rep(c(1L, 3L, NA, 7L, 9L), 1:5)
     xRle <- Rle(x)
     xRleViews <- Views(xRle, start = c(1, 3, 5, 7, 9), end = c(1, 13, 11, 10, 9))
     xList <-
       lapply(seq_len(length(xRleViews)),
              function(i) subseq(x, start = start(xRleViews)[i], end = end(xRleViews)[i]))
-    checkEquals(sapply(xList[-1], min), viewMins(xRleViews[-1]))
-    checkEquals(sapply(xList[-1], min, na.rm = TRUE), viewMins(xRleViews[-1], na.rm = TRUE))
-    checkEquals(sapply(xList[-1], max), viewMaxs(xRleViews[-1]))
-    checkEquals(sapply(xList[-1], max, na.rm = TRUE), viewMaxs(xRleViews[-1], na.rm = TRUE))
+    checkEquals(sapply(xList, min), viewMins(xRleViews))
+    checkEquals(sapply(xList, min, na.rm = TRUE), viewMins(xRleViews, na.rm = TRUE))
+    checkEquals(sapply(xList, max), viewMaxs(xRleViews))
+    checkEquals(sapply(xList, max, na.rm = TRUE), viewMaxs(xRleViews, na.rm = TRUE))
     checkEquals(sapply(xList, sum), viewSums(xRleViews))
     checkEquals(sapply(xList, sum, na.rm = TRUE), viewSums(xRleViews, na.rm = TRUE))
 
-    y <- rep(c(NA, 3.4, NA, 7.8, 9.0), 1:5)
+    y <- rep(c(1.2, 3.4, NA, 7.8, 9.0), 1:5)
     yRle <- Rle(y)
     yRleViews <- Views(yRle, start = c(1, 3, 5, 7, 9), end = c(1, 13, 11, 10, 9))
     yList <-
@@ -25,7 +25,7 @@ test_RleViews <- function() {
     checkEquals(sapply(yList, sum), viewSums(yRleViews))
     checkEquals(sapply(yList, sum, na.rm = TRUE), viewSums(yRleViews, na.rm = TRUE))
 
-    z <- rep(c(NA, 3.4-1i, NA, 7.8+3i, 9.0-2i), 1:5)
+    z <- rep(c(1+1i, 3.4-1i, NA, 7.8+3i, 9.0-2i), 1:5)
     zRle <- Rle(z)
     zRleViews <- Views(zRle, start = c(1, 3, 5, 7, 9), end = c(1, 13, 11, 10, 9))
     zList <-
