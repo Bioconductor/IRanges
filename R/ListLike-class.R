dbfa66dbae93367c252ab0bcd2a82e042797519e
Herve Pages <hpages@fhcrc.org>
2009-02-25 05:50:44 +0000

    Simpler implementation of "sapply" method for ListLike objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@37602 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ListLike-class.R
+++ R/ListLike-class.R
@@ -18,38 +18,19 @@ setMethod("lapply", c("ListLike", "ANY"),
     }
 )
 
-### The implementation of the "sapply" method below was copy-pasted as-is from
-### base::sapply(). Duplicating code is ugly but unfortunately we have to
-### define this generic+method, otherwise, when passed a ListLike object,
-### sapply(X) would call base::sapply() which would call base::lapply() behind
-### the scene and not IRanges::lapply() (generic function implicitly defined by
-### the setMethod statement above).
-### This ugliness could be avoided if base::lapply() itself was a generic.
-### Anybody willing to bring this on R-devel?
+### The implicit generic would dispatch on the X, FUN, simplify and USE.NAMES
+### args but we don't want that.
 setGeneric("sapply", signature=c("X", "FUN"),
     function(X, FUN, ..., simplify=TRUE, USE.NAMES=TRUE)
         standardGeneric("sapply")
 )
+
 setMethod("sapply", c("ListLike", "ANY"),
     function(X, FUN, ..., simplify=TRUE, USE.NAMES=TRUE)
     {
-        FUN <- match.fun(FUN)
-        answer <- lapply(X, FUN, ...)
-        if (USE.NAMES && is.character(X) && is.null(names(answer))) 
-            names(answer) <- X
-        if (simplify && length(answer)
-         && length(common.len <- unique(unlist(lapply(answer, length)))) == 1L)
-        {
-            if (common.len == 1L) 
-                unlist(answer, recursive = FALSE)
-            else if (common.len > 1L) 
-                array(unlist(answer, recursive = FALSE), dim = c(common.len, 
-                    length(X)), dimnames = if (!(is.null(n1 <- names(answer[[1L]])) & 
-                    is.null(n2 <- names(answer)))) 
-                    list(n1, n2))
-            else answer
-        }
-        else answer
+        ii <- seq_len(length(X))
+        names(ii) <- names(X)
+        sapply(ii, function(i) FUN(X[[i]]), ..., simplify=simplify, USE.NAMES=USE.NAMES)
     }
 )
 
