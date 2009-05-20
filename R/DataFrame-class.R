60de1bf711a16a62974ff3d76f46817bbb92417a
Patrick Aboyoun <unknown>
2009-05-20 08:33:50 +0000

    Added a with method for DataFrame classes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@39577 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DataFrame-class.R
+++ R/DataFrame-class.R
@@ -201,3 +201,9 @@ setMethod("eval", c("expressionORlanguage", "DataFrame"),
               }
               eval(expr, env)
           })
+
+setMethod("with", "DataFrame",
+          function(data, expr, ...)
+          {
+              eval(substitute(expr), data, enclos = parent.frame())
+          })
