fda8eaa80220bce29e5b1a9abb18a22de859f4af
Patrick Aboyoun <unknown>
2009-05-22 01:16:08 +0000

    Fixed test name.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@39631 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/unit/List_test.R
+++ tests/unit/List_test.R
@@ -128,7 +128,7 @@ test_List_apply <- function() {
   }
 }
 
-test_IRangesList_annotation <- function() {
+test_List_annotation <- function() {
   int1 <- c(1L,2L,3L,5L,2L,8L)
   int2 <- c(15L,45L,20L,1L,15L,100L,80L,5L)
   for (compress in c(TRUE, FALSE)) {
