c428119ea0bb3bad57ac67353868b7392226a614
Michael Lawrence <lawremi@iastate.edu>
2009-05-24 17:26:11 +0000

    Adding files left out from last commit.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@39673 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/DataFrame-class.R
@@ -0,0 +1,412 @@
+### =========================================================================
+### DataFrame objects
+### -------------------------------------------------------------------------
+
+## A data.frame-like interface for S4 objects that implement length() and `[`
+
+## NOTE: Normal data.frames always have rownames (sometimes as integers),
+## but we allow the rownames to be NULL for efficiency. This means that we
+## need to store the number of rows (nrows).
+setClass("DataFrame",
+         representation(
+                        rownames = "characterORNULL",
+                        nrows = "integer"
+                        ),
+         prototype(rownames = NULL,
+                   nrows = 0L),
+         contains = c("DataTable", "SimpleList"))
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Accessor methods.
+###
+
+setMethod("dim", "DataFrame", function(x) as.integer(c(x@nrows, length(x))))
+
+setMethod("dimnames", "DataFrame",
+          function(x) {
+            rn <- x@rownames
+            if (!is.null(rn)) {
+              rn <- as.character(rn)
+              rn[is.na(rn)] <- "NA"
+              if (any(duplicated(rn)))
+                rn <- make.unique(rn)                
+            }
+            list(rn, names(x))
+          })
+
+
+setReplaceMethod("dimnames", "DataFrame",
+                 function(x, value) {
+                   rows <- value[[1]]
+                   if (!is.null(rows)) {
+                     if (any(is.na(rows)))
+                       stop("missing values not allowed in rownames")
+                     if (length(rows) != nrow(x))
+                       stop("invalid rownames length")
+                     if (any(duplicated(rows)))
+                       stop("duplicate rownames not allowed")
+                     if (!is(rows, "XStringSet"))
+                       rows <- as.character(rows)
+                   }
+                   cols <- value[[2]]
+                   if (!is.null(cols)) {
+                     if (length(cols) > length(x))
+                       stop("more column names than columns")
+                     cols <- make.names(cols, unique=TRUE)
+                   }
+                   x@rownames <- rows
+                   names(x) <- cols
+                   x
+                 })
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Validity.
+###
+
+
+.valid.DataFrame.dim <- function(x)
+{
+  nr <- dim(x)[1]
+  if (!length(nr) == 1)
+    return("length of 'nrows' slot must be 1")
+  if (nr < 0)
+    return("number of rows must be non-negative")
+  NULL
+}
+
+.valid.DataFrame.rownames <- function(x)
+{
+  if (is.null(rownames(x)))
+    return(NULL)
+  if (length(rownames(x)) != nrow(x))
+    return("number of row names and number of rows differ")
+  NULL
+}
+
+.valid.DataFrame.names <- function(x)
+{
+  if (is.null(names(x)))
+    return(NULL)
+  if (any(duplicated(names(x))))
+    return("duplicate column names")
+  if (length(names(x)) != ncol(x))
+    return("number of columns and number of column names differ")
+  NULL
+}
+
+.valid.DataFrame <- function(x)
+{
+  c(.valid.DataFrame.dim(x),
+    .valid.DataFrame.rownames(x),
+    .valid.DataFrame.names(x))
+}
+
+setValidity2("DataFrame", .valid.DataFrame)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Constructor.
+###
+
+DataFrame <- function(..., row.names = NULL)
+{
+  ## build up listData, with names from arguments
+  listData <- list(...)
+  nr <- 0
+  varnames <- character()
+  varlist <- vector("list", length(listData))
+  if (length(listData) > 0) {
+    if (is.null(names(listData)))
+      names(listData) <- character(length(listData))
+    emptynames <- !nzchar(names(listData))
+    args <- as.list(substitute(list(...)))[-1]
+    argdp <- sapply(args, function(arg) deparse(arg)[1])
+    varnames <- as.list(names(listData))
+    nrows <- ncols <- integer(length(varnames))
+    for (i in seq_along(listData)) {
+      element <- listData[[i]]
+      element <- try(as(element, "DataFrame"), silent = TRUE)
+      if (inherits(element, "try-error"))
+        stop("cannot coerce class \"", class(listData[[i]]),
+             "\" to an DataFrame")
+      enames <- names(element)
+      if (ncol(element) > 1) {
+        if (is.null(enames))
+          enames <- seq_len(ncol(element))
+        if (emptynames[i])
+          varnames[[i]] <- enames
+        else varnames[[i]] <- paste(varnames[[i]], enames, sep = ".") 
+      } else {
+        if (!is.null(enames))
+          varnames[[i]] <- enames
+        else if (emptynames[i])
+          varnames[[i]] <- argdp[i]
+      }
+      nrows[i] <- nrow(element)
+      ncols[i] <- ncol(element)
+      varlist[[i]] <- as.list(element, use.names = FALSE)
+    }
+    nr <- max(nrows)
+    for (i in seq_along(listData)) {
+      if (nrows[i] < nr && nrows[i] && (nr %% nrows[i]) == 0) {
+        recycle <- rep(seq_len(nrows[i]), length = nr)
+        varlist[[i]] <- lapply(varlist[[i]], `[`, recycle, drop=FALSE)
+        nrows[i] <- nr
+      }
+    }
+    if (!all(nrows == nr))
+      stop("different row counts implied by arguments")
+    varlist <- unlist(varlist, recursive = FALSE, use.names = FALSE)
+    names(varlist) <- make.names(unlist(varnames[ncols > 0L]), unique = TRUE)
+  }
+  
+  if (!is.null(row.names)) {
+    if (any(is.na(row.names)))
+      stop("missing values in 'row.names'")
+    if (length(varlist) && length(row.names) != nr)
+      stop("invalid length of row names")
+    if (any(duplicated(row.names)))
+      stop("duplicate row names")
+    row.names <- as.character(row.names)
+  }
+  
+  new("DataFrame", listData=varlist, rownames=row.names,
+      nrows=as.integer(max(nr, length(row.names))))
+}
+
+XDataFrame <- function(..., row.names = NULL) {
+  .Deprecated("DataFrame", "IRanges")
+  mc <- match.call()
+  mc[1] <- DataFrame
+  eval(mc)
+}
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Subsetting.
+###
+
+setMethod("[[", "DataFrame",
+          function(x, i, j, ...)
+          {
+            dotArgs <- list(...)
+            if (length(dotArgs) > 0)
+              dotArgs <- dotArgs[names(dotArgs) != "exact"]
+            if (!missing(j) || length(dotArgs) > 0)
+              stop("invalid subsetting")
+            if (missing(i))
+              stop("subscript is missing")
+            if (!is.character(i) && !is.numeric(i))
+              stop("invalid subscript type")
+            if (length(i) < 1L)
+              stop("attempt to select less than one element")
+            if (length(i) > 1L)
+              stop("attempt to select more than one element")
+            if (!is.character(i) && !is.na(i) && (i < 1L || i > length(x)))
+              stop("subscript out of bounds")
+            els <- as.list(x, use.names = FALSE)
+            names(els) <- names(x)
+            els[[i]]
+          }
+          )
+
+setReplaceMethod("[[", "DataFrame",
+                 function(x, i, j,..., value)
+                 {
+                   if (!missing(j) || length(list(...)) > 0)
+                     warning("arguments beyond 'i' ignored")
+                   if (missing(i))
+                     stop("subscript is missing")
+                   if (!is.character(i) && !is.numeric(i))
+                     stop("invalid subscript type")
+                   if (length(i) < 1L)
+                     stop("attempt to select less than one element")
+                   if (length(i) > 1L)
+                     stop("attempt to select more than one element")
+                   if (is.numeric(i) && (i < 1L || i > ncol(x)+1))
+                     stop("subscript out of bounds")
+                   if (!is.null(value) && nrow(x) != length(value)) {
+                     stop("length of data must equal the number of rows")
+                   }
+                   x <- callNextMethod(x, i, value=value)
+                   ## ensure unique, valid names
+                   names(x) <- make.names(names(x), unique=TRUE)
+                   x
+                 })
+
+setMethod("[", "DataFrame",
+          function(x, i, j, ..., drop)
+          {
+            if (length(list(...)) > 0)
+              warning("parameters in '...' not supported")
+
+            checkIndex <- function(i, row = FALSE) {
+              if (row) {
+                nms <- rownames(x)
+                lx <- nrow(x)
+              } else {
+                nms <- names(x)
+                lx <- length(x)
+              }
+              if (!is.atomic(i) && !is(i, "Rle"))
+                return("invalid subscript type")
+              if (!is.null(i) && any(is.na(i)))
+                return("subscript contains NAs")
+              if (is.numeric(i)) {
+                nmi <- i[!is.na(i)]
+                if (any(nmi < -lx) || any(nmi > lx))
+                  return("subscript out of bounds")
+                if (any(nmi < 0) && any(nmi > 0))
+                  return("negative and positive indices cannot be mixed")
+              } else if (is.logical(i)) {
+                if (length(i) > lx)
+                  return("subscript out of bounds")
+              } else if (is.character(i)) {
+                if (is.null(nms))
+                  return("cannot subset by character when names are NULL")
+                if (row)
+                  m <- pmatch(i, nms, duplicates.ok = TRUE)
+                else
+                  m <- match(i, nms)
+                if (!row && any(is.na(m)))
+                  return("mismatching names")
+              } else if (is(i, "Rle")) {
+                if (length(i) > lx)
+                  return("subscript out of bounds")
+              } else if (!is.null(i)) {
+                return("invalid subscript type")
+              }
+              NULL
+            }
+
+            ## no ',' -- forward to list
+            ## NOTE: matrix-style subsetting by logical matrix not supported
+            if ((nargs() - !missing(drop)) < 3) { 
+              if (!missing(drop))
+                warning("parameter 'drop' ignored by list-style subsetting")
+              if (missing(i))
+                return(x)
+              prob <- checkIndex(i)
+              if (!is.null(prob))
+                stop("subsetting as list: ", prob)
+              x <- callNextMethod(x, i)
+              if (any(duplicated(names(x))))
+                names(x) <- make.names(names(x))
+              return(x)
+            }
+
+            dim <- dim(x)
+            if (!missing(j)) {
+              prob <- checkIndex(j)
+              if (!is.null(prob))
+                stop("selecting cols: ", prob)
+              x <- callNextMethod(x, j)
+              if (any(duplicated(names(x))))
+                names(x) <- make.unique(names(x))
+              dim[2] <- length(x)
+            }
+
+            if (!missing(i)) {
+              useI <- TRUE
+              prob <- checkIndex(i, row = TRUE)
+              if (!is.null(prob))
+                stop("selecting rows: ", prob)
+              if (is.character(i)) {
+                i <- pmatch(i, rownames(x), duplicates.ok = TRUE)
+              } else if (is.logical(i)) {
+                if (all(i)) {
+                  useI <- FALSE
+                } else {
+                  i <- which(i)
+                }
+              } else if (is.null(i)) {
+                i <- integer()
+              } else if (is(i, "Rle")) {
+                if (all(runValue(i))) {
+                  useI <- FALSE
+                } else {
+                  i <- which(i)
+                }
+              }
+              if (useI) {
+                x@listData <- lapply(as.list(x), function(y) y[i, drop = FALSE])
+                dim[1] <- length(seq(dim[1])[i]) # may have 0 cols, no rownames
+                x@nrows <- dim[1]
+                x@rownames <- rownames(x)[i]
+              }
+            }
+
+            if (missing(drop)) ## drop by default if only one column left
+              drop <- dim[2] == 1
+            if (drop) {
+              ## one column left
+              if (dim[2] == 1) 
+                return(x[[1]])
+              ## one row left
+              if (dim[1] == 1)
+                return(as(x, "list"))
+            }
+
+            x
+          })
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Coercion.
+###
+
+## Break DataFrame into a normal R data.frame
+setAs("DataFrame", "data.frame",
+      function(from) {
+        as.data.frame(from)
+      })
+
+setMethod("as.data.frame", "DataFrame",
+          function(x, row.names=NULL, optional=FALSE, ...)
+          {
+            if (length(list(...)))
+              stop("arguments in '...' ignored")
+            l <- as(x, "list")
+            if (is.null(row.names))
+              row.names <- rownames(x)
+            if (!length(l) && is.null(row.names))
+              row.names <- seq_len(nrow(x))
+            ## we call as.data.frame here, because data.frame uses S3 dispatch
+            do.call(data.frame, c(lapply(l, as.data.frame, optional = TRUE),
+                                  list(row.names = row.names)))
+          })
+
+## take data.frames to DataFrames
+setAs("data.frame", "DataFrame",
+      function(from) {
+        new("DataFrame", listData = from,
+            nrows = nrow(from), rownames = rownames(from))
+      })
+
+setAs("matrix", "DataFrame", # matrices just go through data.frame
+      function(from) as(as.data.frame(from), "DataFrame"))
+
+setAs("list", "DataFrame", function(from) do.call(DataFrame, from))
+
+## everything else
+setAs("ANY", "DataFrame",
+      function(from) {
+        new("DataFrame", listData = list(from),
+            nrows = as.integer(length(from)))
+      })
+
+### FIXME: only exists due to annoying S4 warning due to its caching of
+### coerce methods.
+setAs("integer", "DataFrame",
+      function(from) {
+        selectMethod("coerce", c("ANY", "DataFrame"))(from)
+      })
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### The "show" method.
+###
+
+setMethod("show", "DataFrame",
+          function(object)
+          {
+            cat("DataFrame object with", nrow(object), "rows and",
+                ncol(object), "columns.\n")
+          })
