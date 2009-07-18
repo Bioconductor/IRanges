6bf1aa117016fb94e2941ba36ef18d7ba1cedbc5
Patrick Aboyoun <unknown>
2009-07-18 07:16:43 +0000

    Further changes to xtabs method for DataTable objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@40696 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DataTable-stats.R
+++ R/DataTable-stats.R
@@ -4,14 +4,9 @@
 
 ## TODO: lm, glm, loess, ...
 
-setGeneric("xtabs", signature = c("formula", "data"),
-           function(formula = ~., data = parent.frame(), subset, na.action,
-                    exclude = c(NA, NaN), drop.unused.levels = FALSE)
-           standardGeneric("xtabs"))
-
-setMethod("xtabs", signature(formula = "ANY", data = "DataTable"),
-          function(formula, data, subset, na.action, exclude,
-                   drop.unused.levels)
+setMethod("xtabs", signature(data = "DataTable"),
+          function(formula = ~., data, subset, na.action, exclude = c(NA, NaN),
+                   drop.unused.levels = FALSE)
           {
             data <- as.env(data)
             callGeneric()
