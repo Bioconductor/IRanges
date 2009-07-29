5c10e62e9e894b86e15f1652384862608608adb5
Patrick Aboyoun <unknown>
2009-07-29 05:17:11 +0000

    Fixed coverage calculation for IRanges objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@40886 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/coverage.c
+++ src/coverage.c
@@ -2,15 +2,17 @@
 #include <stdlib.h> /* for qsort() */
 
 static const int *base_start;
+static const int *base_width;
 
 static int cmp_start_indices_for_ordering(const void *p1, const void *p2)
 {
-	int i1, i2, ret;
+	int i1, i2, ret1, ret2;
 
 	i1 = *((const int *) p1);
 	i2 = *((const int *) p2);
-	ret = *(base_start + i1) - *(base_start + i2);
-	return ret != 0 ? ret : i1 - i2;
+	ret1 = *(base_start + i1) - *(base_start + i2);
+	ret2 = *(base_width + i1) - *(base_width + i2);
+	return ret1 != 0 ? ret1 : ret2;
 }
 
 
@@ -19,9 +21,7 @@ static int cmp_start_indices_for_ordering(const void *p1, const void *p2)
  */
 SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 {
-	int i, j, weight_elt;
-	int index_start, index_end, index_width;
-	const int *index_elt;
+	int i, j;
 	SEXP ans, ans_lengths, ans_values;
 
 	int x_length = _get_IRanges_length(x);
@@ -31,12 +31,12 @@ SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 	// order start values
 	const int *wd, *wt;
 	int order_length = 0;
-	int *order_start = (int *) R_alloc((long) x_length, sizeof(int));
-	memset(order_start, -1, x_length * sizeof(int));
+	int *order = (int *) R_alloc((long) x_length, sizeof(int));
+	memset(order, -1, x_length * sizeof(int));
 	if (LENGTH(weight) == 1) {
 		for (i = 0, wd = x_width; i < x_length; i++, wd++) {
 			if (*wd > 0) {
-				order_start[order_length] = i;
+				order[order_length] = i;
 				order_length++;
 			}
 		}
@@ -44,65 +44,85 @@ SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 		for (i = 0, wd = x_width, wt = INTEGER(weight); i < x_length;
 		     i++, wd++, wt++) {
 			if (*wd > 0 && *wt != 0) {
-				order_start[order_length] = i;
+				order[order_length] = i;
 				order_length++;
 			}
 		}
 	}
-	base_start = x_start;
-	qsort(order_start, order_length, sizeof(int), cmp_start_indices_for_ordering);
 
-	// find buffer size
-	int prev_index = 0;
+	const int *order_elt;
+	int weight_elt, index_start, index_end;
+
+	int *sparse_data;
+	int *sparse_index;
 	int sparse_data_length = 0;
-	for (i = 0, index_elt = order_start; i < order_length; i++, index_elt++)
-	{
-		int curr_start = x_start[*index_elt];
-		int curr_width = x_width[*index_elt];
-		index_start = curr_start > prev_index ? curr_start : prev_index;
-		index_end = curr_start + curr_width - 1;
-		int shift = index_end - index_start + 1;
-		if (shift > 0) {
-			sparse_data_length += shift;
-			prev_index = index_end + 1;
-		}
-	}
-	int *sparse_data = (int *) R_alloc((long) sparse_data_length, sizeof(int));
-	int *sparse_index = (int *) R_alloc((long) sparse_data_length, sizeof(int));
-	memset(sparse_data, 0, sparse_data_length * sizeof(int));
-	memset(sparse_index, 0, sparse_data_length * sizeof(int));
+	if (order_length > 0) {
+		base_start = x_start;
+		base_width = x_width;
+		qsort(order, order_length, sizeof(int), cmp_start_indices_for_ordering);
 
-	// perform coverage calculation
-	int values_length = 0, prev_width = 0;
-	int *prev_sdata, *curr_sdata, *prev_sindex, *curr_sindex;
-	if (sparse_data_length > 0) {
-		int *sparse_data_elt = sparse_data;
-		int *sparse_index_elt = sparse_index;
-		for (i = 0, index_elt = order_start; i < order_length; i++, index_elt++)
+		int prev_index = 0;
+		const int *x_start = INTEGER(_get_IRanges_start(x));
+		const int *x_width = INTEGER(_get_IRanges_width(x));
+		for (i = 0, order_elt = order; i < x_length; i++, order_elt++)
 		{
-			if (*index_elt >= LENGTH(weight)) {
+			if (x_width[*order_elt] == 0)
+				continue;
+			if (*order_elt >= LENGTH(weight)) {
 				weight_elt = INTEGER(weight)[0];
 			} else {
-				weight_elt = INTEGER(weight)[*index_elt];
+				weight_elt = INTEGER(weight)[*order_elt];
 			}
-			index_start = x_start[*index_elt];
-			index_width = x_width[*index_elt];
-			if (*sparse_index_elt < index_start) {
-				int ptr_shift = index_start - *sparse_index_elt;
-				ptr_shift = ptr_shift < prev_width ? ptr_shift : prev_width;
-				if (ptr_shift > 0) {
-					sparse_index_elt += ptr_shift;
-					sparse_data_elt += ptr_shift;
-				}
+			if (weight_elt == 0)
+				continue;
+			index_start = (x_start[*order_elt] > prev_index ? x_start[*order_elt] : prev_index);
+			index_end = x_start[*order_elt] + x_width[*order_elt] - 1;
+			int shift = index_end - index_start + 1;
+			if (shift > 0) {
+				sparse_data_length += shift;
+				prev_index = index_end + 1;
 			}
-			for (j = 0; j < index_width; j++, sparse_index_elt++, sparse_data_elt++, index_start++)
+		}
+	}
+
+	// perform coverage calculation
+	int values_length = 0;
+	int *prev_sdata, *curr_sdata, *prev_sindex, *curr_sindex;
+	if (sparse_data_length > 0) {
+		sparse_data = (int *) R_alloc((long) sparse_data_length, sizeof(int));
+		sparse_index = (int *) R_alloc((long) sparse_data_length, sizeof(int));
+		memset(sparse_data, 0, sparse_data_length * sizeof(int));
+		memset(sparse_index, 0, sparse_data_length * sizeof(int));
+
+		int *sparse_data_elt = sparse_data;
+		int *sparse_index_elt = sparse_index;
+		for (i = 0, order_elt = order; i < x_length; i++, order_elt++)
+		{
+			if (x_width[*order_elt] == 0)
+				continue;
+			if (*order_elt >= LENGTH(weight)) {
+				weight_elt = INTEGER(weight)[0];
+			} else {
+				weight_elt = INTEGER(weight)[*order_elt];
+			}
+			if (weight_elt == 0)
+				continue;
+			index_start = x_start[*order_elt];
+			while (*sparse_index_elt > index_start) {
+				sparse_index_elt--;
+				sparse_data_elt--;
+			}
+			while ((*sparse_index_elt > 0) && (*sparse_index_elt < index_start)) {
+				sparse_index_elt++;
+				sparse_data_elt++;
+			}
+			for (j = 0; j < x_width[*order_elt]; j++, sparse_index_elt++, sparse_data_elt++, index_start++)
 			{
 				*sparse_index_elt = index_start;
 				*sparse_data_elt += weight_elt;
 			}
-			sparse_index_elt -= index_width;
-			sparse_data_elt -= index_width;
-			prev_width = index_width;
+			sparse_index_elt--;
+			sparse_data_elt--;
 		}
 		values_length = 1 + (*sparse_index != 1);
 		for (i = 1, prev_sdata = sparse_data, curr_sdata = (sparse_data + 1),
@@ -173,7 +193,6 @@ SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 	}
 
 	PROTECT(ans = NEW_OBJECT(MAKE_CLASS("Rle")));
-	SET_SLOT(ans, install("vectorLength"), ScalarInteger(INTEGER(width)[0]));
 	SET_SLOT(ans, install("lengths"), ans_lengths);
 	SET_SLOT(ans, install("values"), ans_values);
 	UNPROTECT(3);
