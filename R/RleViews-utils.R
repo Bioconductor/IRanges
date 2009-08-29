03fc7f3b1d7ba35b378dd93f32bf6c3b88190711
Patrick Aboyoun <unknown>
2009-08-29 01:22:25 +0000

    Trimmed RleViews prior to view* operations to prevent view overflow.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@41407 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RleViews-utils.R
+++ R/RleViews-utils.R
@@ -5,6 +5,7 @@
 
 setMethod("viewApply", "RleViews",
           function(X, FUN, ..., simplify = TRUE) {
+              X <- trim(X)
               ans <-
                 aggregate(subject(X), start = structure(start(X), names = names(X)),
                           end = end(X), FUN = FUN, ..., simplify = simplify)
@@ -19,37 +20,37 @@ setGeneric("viewMins", signature="x",
            function(x, na.rm = FALSE) standardGeneric("viewMins"))
 setMethod("viewMins", "RleViews",
           function(x, na.rm = FALSE)
-          .Call("RleViews_viewMins", x, na.rm, PACKAGE="IRanges"))
+          .Call("RleViews_viewMins", trim(x), na.rm, PACKAGE="IRanges"))
 
 setGeneric("viewMaxs", signature="x",
            function(x, na.rm = FALSE) standardGeneric("viewMaxs"))
 setMethod("viewMaxs", "RleViews",
           function(x, na.rm = FALSE)
-          .Call("RleViews_viewMaxs", x, na.rm, PACKAGE="IRanges"))
+          .Call("RleViews_viewMaxs", trim(x), na.rm, PACKAGE="IRanges"))
 
 setGeneric("viewSums", signature="x",
            function(x, na.rm = FALSE) standardGeneric("viewSums"))
 setMethod("viewSums", "RleViews",
           function(x, na.rm = FALSE)
-          .Call("RleViews_viewSums", x, na.rm, PACKAGE="IRanges"))
+          .Call("RleViews_viewSums", trim(x), na.rm, PACKAGE="IRanges"))
 
 setGeneric("viewWhichMins", signature="x",
            function(x, na.rm = FALSE) standardGeneric("viewWhichMins"))
 setMethod("viewWhichMins", "RleViews",
           function(x, na.rm = FALSE)
-          .Call("RleViews_viewWhichMins", x, na.rm, PACKAGE="IRanges"))
+          .Call("RleViews_viewWhichMins", trim(x), na.rm, PACKAGE="IRanges"))
 
 setGeneric("viewWhichMaxs", signature="x",
            function(x, na.rm = FALSE) standardGeneric("viewWhichMaxs"))
 setMethod("viewWhichMaxs", "RleViews",
           function(x, na.rm = FALSE)
-          .Call("RleViews_viewWhichMaxs", x, na.rm, PACKAGE="IRanges"))
+          .Call("RleViews_viewWhichMaxs", trim(x), na.rm, PACKAGE="IRanges"))
 
 setGeneric("viewRangeMaxs",
            function(x, na.rm = FALSE) standardGeneric("viewRangeMaxs"))
 setMethod("viewRangeMaxs", "RleViews",
           function(x, na.rm = FALSE) {
-              maxs <- viewWhichMaxs(x, na.rm = na.rm)
+              maxs <- viewWhichMaxs(trim(x), na.rm = na.rm)
               if (any(is.na(maxs)))
                   stop("missing values present, set 'na.rm = TRUE'")
               findRange(maxs, subject(x))
@@ -59,7 +60,7 @@ setGeneric("viewRangeMins",
            function(x, na.rm = FALSE) standardGeneric("viewRangeMins"))
 setMethod("viewRangeMins", "RleViews",
           function(x, na.rm = FALSE) {
-              mins <- viewWhichMins(x, na.rm = na.rm)
+              mins <- viewWhichMins(trim(x), na.rm = na.rm)
               if (any(is.na(mins)))
                   stop("missing values present, set 'na.rm = TRUE'")
               findRange(mins, subject(x))
