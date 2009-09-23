f5e61d9f9168942abef1074ee1b75e70c16c6b58
Herve Pages <hpages@fhcrc.org>
2009-09-23 03:07:22 +0000

    "copy" methods for SharedRaw and SharedVector_Pool objects now supports the start=NA/end=NA/width=NA interface.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@41885 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/copy.R
+++ R/copy.R
@@ -1,41 +1,37 @@
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### The "copy" generic and methods.
+### The copy() generic and its methods.
 ###
 
 setGeneric("copy", function(x, ...) standardGeneric("copy"))
 
 setMethod("copy", "SharedRaw",
-    function(x, lkup=NULL, reverse=FALSE)
+    function(x, start=NA, end=NA, width=NA, lkup=NULL, reverse=FALSE)
     {
-        ans_length <- length(x)
+        solved_SEW <- solveUserSEW(length(x), start=start, end=end, width=width)
+        if (!isTRUEorFALSE(reverse))
+            stop("'reverse' must be TRUE or FALSE")
+        ans_length <- width(solved_SEW)
         ans <- SharedRaw(ans_length)
-        if (is.null(lkup)) {
-            if (reverse)
-                .Call("SharedRaw_reverse_copy_from_i1i2",
-                      ans, x, 1L, ans_length, PACKAGE="IRanges")
-            else
-                .Call("SharedRaw_copy_from_i1i2",
-                      ans, x, 1L, ans_length, PACKAGE="IRanges")
-        } else {
-            if (reverse)
-                .Call("SharedRaw_reverse_translate_copy_from_i1i2",
-                      ans, x, 1L, ans_length, lkup, PACKAGE="IRanges")
-            else
-                .Call("SharedRaw_translate_copy_from_i1i2",
-                      ans, x, 1L, ans_length, lkup, PACKAGE="IRanges")
-        }
+        if (reverse)
+            SharedRaw.reverseCopy(ans, start(solved_SEW), end(solved_SEW),
+                                  src=x, lkup=lkup)
+        else
+            SharedRaw.copy(ans, start(solved_SEW), end(solved_SEW),
+                           src=x, lkup=lkup)
         return(ans)
     }
 )
 
 setMethod("copy", "SharedVector_Pool",
-    function(x, lkup=NULL, reverse=FALSE)
+    function(x, start=NA, end=NA, width=NA, lkup=NULL, reverse=FALSE)
     {
         ## FIXME: This limitation must be temporary only. We need copy()
         ## to work on a SharedVector_Pool object of arbitrary length!
         if (length(x) != 1L)
             stop("IRanges internal error: length(x) != 1")
-        as(copy(x[[1L]], lkup=lkup, reverse=reverse), "SharedVector_Pool")
+        as(copy(x[[1L]], start=start, end=end, width=width,
+                lkup=lkup, reverse=reverse),
+           "SharedVector_Pool")
     }
 )
 
