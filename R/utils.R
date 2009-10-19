c59005816b6f02127cc5bce3617d1fd7c7a820d2
Patrick Aboyoun <unknown>
2009-10-19 16:35:02 +0000

    Speedup to ellispize when obj is long.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@42478 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -286,22 +286,25 @@ labeledLine <- function(label, els, count = TRUE, sep = " ", ellipsis = "...") {
 ellipsize <- function(obj, width = getOption("width"), sep = " ",
                       ellipsis = "...")
 {
+  if (length(obj) > 2 * width)
+    obj <- c(head(obj, width), tail(obj, width))
   str <- encodeString(obj)
   ## get order selectSome() would print
   half <- seq_len(ceiling(length(obj) / 2))
-  ind <- t(cbind(half, length(obj) - half + 1))[seq_along(obj)]
+  ind <- as.vector(rbind(half, length(obj) - half + 1))
   nc <- cumsum(nchar(str[ind]) + nchar(sep)) - nchar(sep)
   last <- findInterval(width, nc)
   if (length(obj) > last) {
     ## make sure ellipsis fits
     while (last && (nc[last] + nchar(sep)*2^(last>1) + nchar(ellipsis)) > width)
-      last <- last - 1
+      last <- last - 1L
     if (last == 0) ## have to truncate the first element
       str <- paste(substring(str[1], 1, width - nchar(ellipsis)), ellipsis,
                    sep = "")
     else if (last == 1) ## can only show the first
       str <- c(str[1], "...")
-    else str <- selectSome(str, last+1)
+    else
+      str <- selectSome(str, last + 1L)
   }
   paste(str, collapse = sep)
 }
@@ -318,8 +321,9 @@ selectSome <- function (obj, maxToShow = 5)
     top <- len - (maxToShow - bot - 1)
     nms <- obj[c(1:bot, top:len)]
     c(as.character(nms[1:bot]), "...", as.character(nms[-c(1:bot)]))
+  } else {
+    obj
   }
-  else obj
 }
 
 mseq <- function(from, to) {
