2a265ee57d9ae205296fe48856f50b95a34add4c
Patrick Aboyoun <unknown>
2010-01-22 07:50:50 +0000

    Reverting to previous version.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@44152 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/coverage.c
+++ src/coverage.c
@@ -94,6 +94,8 @@ SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 		curr_weight = 0;
 		if (weight_length == 1) {
 			for (i = 0, order_elt = order; i < order_length; i++, order_elt++) {
+				if (i % 100000 == 99999)
+					R_CheckUserInterrupt();
 				index = *order_elt / 2;
 				is_end = *order_elt % 2;
 				if (is_end) {
@@ -113,6 +115,8 @@ SEXP IRanges_coverage(SEXP x, SEXP weight, SEXP width)
 			}
 		} else {
 			for (i = 0, order_elt = order; i < order_length; i++, order_elt++) {
+				if (i % 100000 == 99999)
+					R_CheckUserInterrupt();
 				index = *order_elt / 2;
 				is_end = *order_elt % 2;
 				if (is_end) {
