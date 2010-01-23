8f0646fb49eb48f501d47c47a158bc7c9b59ad50
Patrick Aboyoun <unknown>
2010-01-23 18:26:28 +0000

    Added drop argument to findOverlap RangedData methods.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@44201 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RangedData-utils.R
+++ R/RangedData-utils.R
@@ -60,24 +60,27 @@ setMethod("rdapply", "RDApplyParams", function(x) {
 
 setMethod("findOverlaps", c("RangedData", "RangedData"),
           function(query, subject, maxgap = 0, multiple = TRUE,
-                   type = c("any", "start", "end", "within", "equal"))
+                   type = c("any", "start", "end", "within", "equal"),
+                   drop = FALSE)
           {
             findOverlaps(ranges(query), ranges(subject), maxgap = maxgap,
-                         multiple = multiple, type = type)
+                         multiple = multiple, type = type, drop = drop)
           })
 setMethod("findOverlaps", c("RangedData", "RangesList"),
           function(query, subject, maxgap = 0, multiple = TRUE,
-                   type = c("any", "start", "end", "within", "equal"))
+                   type = c("any", "start", "end", "within", "equal"),
+                   drop = FALSE)
           {
             findOverlaps(ranges(query), subject, maxgap = maxgap,
-                         multiple = multiple, type = type)
+                         multiple = multiple, type = type, drop = drop)
           })
 setMethod("findOverlaps", c("RangesList", "RangedData"),
           function(query, subject, maxgap = 0, multiple = TRUE,
-                   type = c("any", "start", "end", "within", "equal"))
+                   type = c("any", "start", "end", "within", "equal"),
+                   drop = FALSE)
           {
             findOverlaps(query, ranges(subject), maxgap = maxgap,
-                         multiple = multiple, type = type)
+                         multiple = multiple, type = type, drop = drop)
           })
 
 setMethod("%in%", c("RangedData", "RangedData"),
