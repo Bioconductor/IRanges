528cb048d42a7c86f12a39dcfebbdadb977ddeb1
Patrick Aboyoun <unknown>
2010-02-10 01:09:36 +0000

    Minor tweak to [<- method for Rle objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@44602 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Rle-class.R
+++ R/Rle-class.R
@@ -376,7 +376,7 @@ setMethod("[", "Rle",
 
 setReplaceMethod("[", "Rle",
                  function(x, i, j,..., value) {
-                     if (length(value) <= 1) {
+                     if (is.null(value) || length(value) == 1) {
                          if (missing(i))
                              output <- callNextMethod(x = x, value = value)
                          else
