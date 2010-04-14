50938bf28ad0a7eec8708248595f6b918f1d5f77
Michael Lawrence <lawremi@iastate.edu>
2010-04-14 04:26:03 +0000

    Add regression test for the as.matrix,RangesMatchingList bug.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@45985 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_RangesMatchingList.R
@@ -0,0 +1,6 @@
+test_RangesMatchingList_as_matrix <- function() {
+  x <- RangedData(IRanges(start=c(1,6), end=c(5,10)), space=c("chr1","chr2"))
+  y <- RangedData(IRanges(start=8, end=10), space="chr2")
+  checkIdentical(as.matrix(findOverlaps(x, y)),
+                 cbind(query = 2L, subject = 1L))
+}
