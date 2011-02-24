74364a6db2c978cde37db9342df07b40f1054816
Herve Pages <hpages@fhcrc.org>
2011-02-24 07:28:56 +0000

    isConstant() now properly handles NAs in integer vectors and does its best to handle NAs, NaNs and infinite values in numeric vectors in a way that "makes sense".
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@53244 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/isConstant.R
+++ R/isConstant.R
@@ -20,12 +20,79 @@ setGeneric("isConstant", function(x) standardGeneric("isConstant"))
 ### than isConstant6().
 ### Results obtained on 'x0 <- rep.int(112L, 999999L)' with R-2.13 Under
 ### development (unstable) (2011-01-08 r53945).
+
+### For this method we use a modified version of isConstant5() above that
+### handles NAs.
 setMethod("isConstant", "integer",
-    function(x) {length(x) <= 1L || min(x) == max(x)}
+    function(x)
+    {
+        if (length(x) <= 1L)
+            return(TRUE)
+        x_min <- min(x, na.rm=FALSE)
+        if (!is.na(x_min))  # success means 'x' contains no NAs
+            return(x_min == max(x, na.rm=FALSE))
+        ## From here 'x' is guaranteed to have a length >= 2 and to contain
+        ## at least an NA.
+        ## 'min(x, na.rm=TRUE)' issues a warning if 'x' contains only NAs.
+        ## In that case, and in that case only, it returns Inf.
+        x_min <- suppressWarnings(min(x, na.rm=TRUE))
+        if (x_min == Inf)
+            return(NA)
+        ## From here 'x' is guaranteed to contain a mix of NAs and non-NAs.
+        x_max <- max(x, na.rm=TRUE)
+        if (x_min == x_max)
+            return(NA)
+        FALSE
+    }
 )
 
+### Like the method for integer vectors this method also uses a comparison
+### between min(x) and max(x). In addition it needs to handle rounding errors
+### and special values: NA, NaN, Inf and -Inf.
 ### Using all.equal() ensures that TRUE is returned on c(11/3, 2/3+4/3+5/3).
 setMethod("isConstant", "numeric",
-    function(x) {length(x) <= 1L || isTRUE(all.equal(min(x), max(x)))}
+    function(x)
+    {
+        if (length(x) <= 1L)
+            return(TRUE)
+        x_min <- min(x, na.rm=FALSE)
+        if (!is.na(x_min)) {  # success means 'x' contains no NAs and no NaNs
+            x_max <- max(x, na.rm=FALSE)
+            if (is.finite(x_min) && is.finite(x_max))
+                return(isTRUE(all.equal(x_min, x_max)))
+            if (x_min == x_max)  # both are Inf or both are -Inf
+                return(NA)
+            return(FALSE)
+        }
+        ## From here 'x' is guaranteed to have a length >= 2 and to contain
+        ## at least an NA or NaN.
+        ## 'min(x, na.rm=TRUE)' issues a warning if 'x' contains only NAs
+        ## and NaNs.
+        x_min <- suppressWarnings(min(x, na.rm=TRUE))
+        if (x_min == Inf) {
+            ## Only possible values in 'x' are NAs, NaNs or Infs.
+            is_in_x <- c(NA, NaN, Inf) %in% x
+            if (is_in_x[2L] && is_in_x[3L])
+                return(FALSE)
+            return(NA)
+        }
+        ## From here 'x' is guaranteed to contain at least one value that is
+        ## not NA or NaN or Inf.
+        x_max <- max(x, na.rm=TRUE)
+        if (x_max == -Inf) {
+            ## Only possible values in 'x' are NAs, NaNs or -Infs.
+            is_in_x <- c(NA, NaN, -Inf) %in% x
+            if (is_in_x[2L] && is_in_x[3L])
+                return(FALSE)
+            return(NA)
+        }
+        if (is.infinite(x_min) || is.infinite(x_max))
+            return(FALSE)
+        if (!isTRUE(all.equal(x_min, x_max)))
+            return(FALSE)
+        if (NaN %in% x)
+            return(FALSE)
+        return(NA)
+    }
 )
 
