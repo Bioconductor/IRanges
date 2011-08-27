32cba77bd1a846457d5b20b88a973a613d963443
Michael Lawrence <lawremi@iastate.edu>
2011-08-27 13:37:28 +0000

    Use as.data.frame in xtabs,DataFrame so that it reduces everything to a vector.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@57685 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DataTable-stats.R
+++ R/DataTable-stats.R
@@ -8,7 +8,7 @@ setMethod("xtabs", signature(data = "DataTable"),
           function(formula = ~., data, subset, na.action, exclude = c(NA, NaN),
                    drop.unused.levels = FALSE)
           {
-            data <- as.env(data)
+            data <- as.data.frame(data)
             callGeneric()
           })
 
