c8782ddedb9ffa422189917d9cfbfb76bebc09f7
Herve Pages <hpages@fhcrc.org>
2011-10-12 04:35:42 +0000

    A silly hack to get rid of a silly 'R CMD check' WARNING (seen on the BSgenome data packages, looks like an issue with the methods package not being fully functional if imported but not explicitly loaded in the .onLoad hook).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@59107 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,42 @@
 
 .onLoad <- function(libname, pkgname)
 {
-    ## The purpose of the hack below is to fix the prototypes of the following
-    ## classes: SharedRaw, SharedInteger, SharedDouble, XRaw, XInteger and
-    ## XDouble. Without this hack, calling new() on any of those classes
-    ## (with e.g. 'new("SharedRaw")') returns an invalid object.
+    ## -- FIRST HACK --
+    ## Not loading the methods package can cause some strange 'R CMD check'
+    ## WARNINGs. For example, on a BSgenome data package (with R 2.14.0):
+    ##
+    ##   * checking whether the namespace can be loaded with stated dependencies ... WARNING
+    ##   Error: .onLoad failed in loadNamespace() for ‘BSgenome.Celegans.UCSC.ce2’, details:
+    ##     call: length(x)
+    ##     error: could not find function "loadMethod"
+    ##   Execution halted
+    ##
+    ##   A namespace must be able to be loaded with just the base namespace
+    ##   loaded: otherwise if the namespace gets loaded by a saved object, the
+    ##   session will be unable to start.
+    ##
+    ##   Probably some imports need to be declared in the NAMESPACE file.
+    ##
+    ## However, loading the methods package with library(methods) produces the
+    ## following 'R CMD check' NOTE (with R 2.14.0):
+    ##
+    ##   * checking R code for possible problems ... NOTE
+    ##   File ‘IRanges/R/zzz.R’:
+    ##     .onLoad calls:
+    ##       library(methods)
+    ##
+    ##   Package startup functions should not change the search path.
+    ##   See section ‘Good practice’ in ?.onAttach.
+    ##
+    ## So we cheat on codetools to avoid this NOTE.
+    sillyname <- library
+    sillyname(methods)
+
+    ## -- SECOND HACK --
+    ## The purpose of this 2nd hack below is to fix the prototypes of the
+    ## following classes: SharedRaw, SharedInteger, SharedDouble, XRaw,
+    ## XInteger and XDouble. Without this hack, calling new() on any of those
+    ## classes (with e.g. 'new("SharedRaw")') returns an invalid object.
     ## In order to "fix" those prototypes, we cannot use the standard
     ## mechanism (which is to specify default slot values in the prototype
     ## part of the setClass() statements) because the DLL of the package needs
@@ -46,3 +78,4 @@
 {
     library.dynam.unload("IRanges", libpath)
 }
+
