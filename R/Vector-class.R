317bd09448ef897760afe5a4b65267a1ee0be144
Herve Pages <hpages@fhcrc.org>
2011-11-11 02:31:40 +0000

    Moved rep.int() generic to the BiocGenerics package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@60230 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Vector-class.R
+++ R/Vector-class.R
@@ -648,22 +648,6 @@ setMethod("rev", "Vector",
 setMethod("rep", "Vector", function(x, ...)
           x[rep(seq_len(length(x)), ...)])
 
-### The natural (and cleaner) thing to do for this generic would be to use
-### the same arguments as base::rep.int() (i.e. x, times) but then
-### 'R CMD check' would get confused and think that we are trying to define
-### an S3 method for base::rep() and would complain (observed with R <= 2.12):
-###   * checking S3 generic/method consistency ... WARNING
-###   rep:
-###     function(x, ...)
-###   rep.int:
-###     function(x, times)
-### so we use the arguments of base::rep() (i.e. x, ...) just to make
-### 'R CMD check' happy.
-setGeneric("rep.int",
-    function(x, ...) standardGeneric("rep.int"),
-        useAsDefault = function(x, ...) base::rep.int(x, ...)
-)
-
 setMethod("rep.int", "Vector",
     function(x, times) x[rep.int(seq_len(length(x)), times)]
 )
