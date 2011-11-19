b536b4d15c8ac98c34b52065d9e2ae68d8883006
Michael Lawrence <lawremi@iastate.edu>
2011-11-19 13:48:53 +0000

    Add RangesMapping class for representing mapping between coordinate spaces.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@60520 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/RangesMapping-class.R
@@ -0,0 +1,53 @@
+### =========================================================================
+### RangesMapping objects
+### -------------------------------------------------------------------------
+###
+### A RangesMapping encodes a mapping of a set of ranges to some other
+### coordinate space.
+###
+
+## Conceptually, a RangesMapping is a matching of each query range to
+## one or more elements in a subject. The geometry of the query range
+## is then transformed according to that matching. Thus, this data
+## class combines a RangesMatching with a set of transformed ranges.
+
+## In IRanges, we do not have any tabular structure that links a space
+## with an interval, except for RangedData. We could use a RangedData
+## in place of this class, but that might be too low-level. Instead,
+## we have an accessor for each, and support a coercion.
+
+setClass("RangesMapping",
+         representation(matching = "RangesMatching", space = "Rle",
+                        ranges = "Ranges"))
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Accessors
+###
+
+matching <- function(x) x@matching
+
+setMethod("space", "RangesMapping", function(x) x@space)
+
+setMethod("ranges", "RangesMapping", function(x) x@ranges)
+
+setMethod("dim", "RangesMapping", function(x) dim(matching(x)))
+
+setMethod("length", "RangesMapping", function(x) length(ranges(x)))
+
+setMethod("subjectHits", "RangesMapping", function(x) subjectHits(matching(x)))
+
+setMethod("queryHits", "RangesMapping", function(x) queryHits(matching(x)))
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Coercion
+###
+
+setAs("RangesMapping", "RangedData", function(from) {
+  RangedData(ranges(from), space = space(from), as.matrix(matching(from)))
+})
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### The 'map' generic, methods of which produce instances of this class
+###
+
+setGeneric("map", function(from, to, ...) standardGeneric("map"))
