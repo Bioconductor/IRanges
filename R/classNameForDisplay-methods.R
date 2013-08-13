71e707e44a2963354a9b242c264ec267e5d4a492
Herve Pages <hpages@fhcrc.org>
2013-08-13 19:28:08 +0000

    Tweak to default "classNameForDisplay" method so it always returns a single string.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@79409 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/classNameForDisplay-methods.R
+++ R/classNameForDisplay-methods.R
@@ -8,7 +8,10 @@ setGeneric("classNameForDisplay",
 setMethod("classNameForDisplay", "ANY",
    function(x)
    {
-       class(x)
+       ## Selecting the 1st element guarantees that we return a single string
+       ## (e.g. on an ordered factor, class(x) returns a character vector of
+       ## length 2).
+       class(x)[1L]
    }
 )
 
