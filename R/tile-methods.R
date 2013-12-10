c2a64277777e56755676a162a10847bc24a9ec18
Michael Lawrence <lawremi@iastate.edu>
2013-12-10 21:29:14 +0000

    add tile,Ranges method that splits ranges into tiles according to the UCSC (Jim Kent) convention, which is subtly different from tileGenome()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@84127 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/tile-methods.R
@@ -0,0 +1,37 @@
+### =========================================================================
+### "tile" methods
+### -------------------------------------------------------------------------
+###
+
+setGeneric("tile", function(x, n, width, ...) standardGeneric("tile"))
+
+setMethod("tile", "Ranges", function(x, n, width) {
+  if (!missing(n)) {
+    if (!missing(width))
+      stop("only one of 'n' and 'width' can be specified")
+    if (any(IRanges::width(x) < n))
+      stop("some width(x) are less than 'n'")
+    if (any(n < 0L))
+      stop("some 'n' are negative")
+    n <- recycleVector(n, length(x))
+  }
+  if (!missing(width)) {
+    if (!missing(n))
+      stop("only one of 'n' and 'width' can be specified")
+    if (any(width(x) < width))
+      stop("some width(x) are less than 'width'")
+    if (any(width < 0L))
+      stop("some 'width' are negative")
+    n <- ceiling(width(x) / width)
+  }
+  width <- IRanges::width(x) / n
+  ## The floor() is itentional for compatibility with Jim Kent's BigWig code
+  ## tileGenome() uses ceiling() instead
+  tile.end <- floor(as.integer(IRanges(rep(1L, length(n)), width=n)) *
+                    rep(width, n))
+  tile.end.abs <- tile.end + rep(start(x), n) - 1L
+  tile.width <- diffWithInitialZero(as.integer(tile.end.abs))
+  p <- PartitioningByWidth(n)
+  tile.width[start(p)] <- tile.end[start(p)]
+  relist(IRanges(width=tile.width, end=tile.end.abs), p)
+})
---
+++ inst/unitTests/test_tile-methods.R
@@ -0,0 +1,27 @@
+test_tile <- function() {
+  ir <- IRanges()
+  checkIdentical(tile(ir, n=3), IRangesList())
+  checkIdentical(tile(ir, width=2), IRangesList())
+  checkIdentical(tile(ir, n=0), IRangesList())
+  ir <- IRanges(1, 4)
+  checkIdentical(tile(ir, n=2), IRangesList(IRanges(c(1, 3), c(2, 4))))
+  checkIdentical(tile(ir, n=2), tile(ir, width=2))
+  ir <- IRanges(1, 5)
+  checkIdentical(tile(ir, n=3), IRangesList(IRanges(c(1, 2, 4), c(1, 3, 5))))
+  checkIdentical(tile(ir, n=3), tile(ir, width=2))
+  ir <- IRanges(1, 4)
+  checkIdentical(tile(ir, n=3), IRangesList(IRanges(1:3, c(1, 2, 4))))
+  ir <- IRanges(1:3, width=5:3)
+  checkIdentical(tile(ir, n=3),
+                 IRangesList(IRanges(c(1, 2, 4), c(1, 3, 5)),
+                             IRanges(c(2, 3, 4), c(2, 3, 5)),
+                             IRanges(c(3, 4, 5), c(3, 4, 5))))
+  checkIdentical(tile(ir, width=2),
+                 IRangesList(IRanges(c(1, 2, 4), c(1, 3, 5)),
+                             IRanges(c(2, 4), c(3, 5)),
+                             IRanges(c(3, 4), c(3, 5))))
+  checkException(tile(ir, n=4), silent=TRUE)
+  checkException(tile(ir, width=4), silent=TRUE)
+  checkException(tile(ir, width=-1), silent=TRUE)
+  checkException(tile(ir, n=-1), silent=TRUE)
+}
