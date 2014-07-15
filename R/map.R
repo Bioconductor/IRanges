3860c662ec1238193297640ccc065bc1a03a8402
Valerie Obenchain <vobencha@fhcrc.org>
2014-07-15 02:09:43 +0000

    make map/pmap defunct instead of deprecated (skipping deprecation step)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@92496 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/map.R
+++ R/map.R
@@ -3,7 +3,7 @@
 ### -------------------------------------------------------------------------
 ###
 
-## Methods for 'map' in GenomicRanges are deprecated.
+## Methods for 'map' in GenomicRanges are defunct.
 ## Use 'mapCoords' and 'pmapCoords' instead.
 
 setGeneric("map", function(from, to, ...) standardGeneric("map"))
