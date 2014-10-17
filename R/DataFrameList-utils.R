c31046046bb022627881bb5d9a7df98a41f48af8
Michael Lawrence <lawremi@iastate.edu>
2014-10-17 02:44:03 +0000

    add a transform,SplitDataFrameList; acts like transform,DataFrame, which requires some gymnastics, but allows convenient group-wise summaries and transformations
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@95665 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DataFrameList-utils.R
+++ R/DataFrameList-utils.R
@@ -15,6 +15,39 @@ setMethod("rbind", "DataFrameList",
 setMethod("stack", "DataFrameList",
           function(x, index.var = "name")
           {
-            DataFrame(.stack.ind(x, index.var), x,
+            DataFrame(.stack.ind(x, index.var), unlist(x, use.names=FALSE),
                       row.names = unlist(lapply(x, rownames)))
           })
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Transforming.
+###
+
+setClass("SDFLWrapperForTransform",
+         representation(delegate = "SplitDataFrameList"),
+         contains="Vector")
+
+setMethod("colnames", "SDFLWrapperForTransform", function(x) {
+  commonColnames(x@delegate)
+})
+
+setMethod("[[", "SDFLWrapperForTransform", function (x, i, j, ...) {
+  x@delegate[,i]
+})
+
+setReplaceMethod("[[", "SDFLWrapperForTransform", function(x, i, j, ..., value) {
+  x@delegate[,i] <- value
+  x
+})
+
+setMethod("as.env", "SDFLWrapperForTransform", function(x, ...) {
+  env <- selectMethod(as.env, "DataTable")(x, ...)
+  S4Vectors:::addSelfRef(x@delegate, env)
+})
+
+transform.SplitDataFrameList <- function(`_data`, ...) {
+  illConceivedWrapper <- new("SDFLWrapperForTransform", delegate=`_data`)
+  S4Vectors:::transform.DataTable(illConceivedWrapper, ...)@delegate
+}
+
+setMethod("transform", "SplitDataFrameList", transform.SplitDataFrameList)
