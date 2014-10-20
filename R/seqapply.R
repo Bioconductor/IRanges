3b2ed526e7940879569fd373673ce6a76999105d
Herve Pages <hpages@fhcrc.org>
2014-10-20 19:28:06 +0000

    seqapply, seqby, seqsplit, etc now defunct (deprecated in BioC 3.0)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@95801 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/seqapply.R
+++ R/seqapply.R
@@ -3,116 +3,24 @@
 ### -------------------------------------------------------------------------
 ###
 
-## FIXME: these functions should probably be deprecated; we no longer
-## use the term 'sequence' in the Vector API, and the wrappers are
-## trivially simple.
-
-.asList <- function(x, ...) {
-  if (is(x, "List"))
-    return(x)
-  if (!is.list(x))
-    stop("'x' must be a 'list'")
-  cl <- lapply(x, class)
-  clnames <- unique(unlist(cl, use.names=FALSE))
-  cons <- SimpleList
-  if (length(clnames) == 1L) {
-    cl <- cl[[1]]
-    pkg <- packageSlot(cl)
-  } else if (length(clnames)) {
-    contains <- lapply(cl, function(x) getClass(x, TRUE)@contains)
-    clnames <- c(clnames,
-                 unlist(lapply(contains, names), use.names=FALSE))
-    contab <- table(factor(clnames, unique(clnames)))
-    cl <- names(contab)[contab == length(x)]
-    if (length(cl))
-      pkg <- sapply(do.call(c, unname(contains))[cl], packageSlot)
-  }
-  if (length(cl)) {
-    constructorName <- function(x) {
-      substring(x, 1, 1) <- toupper(substring(x, 1, 1))
-      paste(x, "List", sep = "")
-    }
-    if (is.null(pkg))
-      ns <- topenv()
-    else ns <- getNamespace(pkg[1])
-    consym <- constructorName(cl[1])
-    if (exists(consym, ns))
-      cons <- get(consym, ns)
-    else {
-      if (length(cl) == 1L) {
-        contains <- getClass(cl, TRUE)@contains
-        cl <- names(contains)
-        pkg <- sapply(contains, packageSlot)
-      } else {
-        cl <- tail(cl, -1)
-        pkg <- tail(pkg, -1)
-      }
-      if (length(cl)) {
-        if (!length(pkg))
-          ns <- list(topenv())
-        connms <- constructorName(cl)
-        ns <- lapply(pkg, getNamespace)
-        coni <- head(which(mapply(exists, connms, ns)), 1)
-        if (length(coni))
-          cons <- get(connms[coni], ns[[coni]])
-      }
-    }
-  }
-  cons(x, ...)
-}
-
-.mapply_List <- function(FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE,
-                        USE.NAMES = TRUE)
-{
-    seqs <- list(...)
-    isListOrVector <- function(x) is.vector(x) | is(x, "List")
-    if (any(!sapply(seqs, isListOrVector)))
-        stop("all objects in ... should be a vector or 'List'")
-    elens <- sapply(seqs, length) ## elementLengths uses NROW, inappropriate
-    if (any(elens == 0L))
-      return(list())
-    N <- max(elens)
-    if (any(N %% elens != 0L))
-        stop("all object lengths must be multiple of longest object length")
-    recycleExtract <- function(x, i) x[[(i - 1L) %% length(x) + 1L]]
-    FUNprime <- function(.__INDEX__, ...) {
-        do.call(FUN, c(lapply(seqs, recycleExtract, .__INDEX__), ...))
-    }
-    nms <- names(seqs[[1]])
-    if (is.null(nms) && is.character(seqs[[1]]))
-      nms <- seqs[[1]]
-    mapply(FUNprime, structure(seq_len(N), names = nms),
-           MoreArgs = MoreArgs, SIMPLIFY = SIMPLIFY,
-           USE.NAMES = USE.NAMES)
-}
-
 seqapply <- function(X, FUN, ...) {
-  .Deprecated("as(lapply(X, FUN, ...), 'List')")
-  .asList(lapply(X, FUN, ...))
+  .Defunct("as(lapply(X, FUN, ...), 'List')")
 }
 
 mseqapply <- function(FUN, ..., MoreArgs = NULL, USE.NAMES = TRUE) {
-  .Deprecated("as(mapply(FUN, ...), 'List')")
-  .asList(.mapply_List(FUN, ..., MoreArgs = MoreArgs, SIMPLIFY = FALSE,
-                       USE.NAMES = USE.NAMES))
+  .Defunct("as(mapply(FUN, ...), 'List')")
 }
 
 tseqapply <- function(X, INDEX, FUN = NULL, ...) {
-  .Deprecated("as(tapply(X, INDEX, FUN, ...), 'List')")
-  .asList(tapply(X, INDEX, FUN, ..., simplify = FALSE))
+  .Defunct("as(tapply(X, INDEX, FUN, ...), 'List')")
 }
 
 seqsplit <- function(x, f, drop=FALSE) {
-  .Deprecated("splitAsList")
-  ans_class <- try(relistToClass(x), silent=TRUE)
-  if (inherits(ans_class, "try-error"))
-    return(.asList(split(x, f, drop)))
-  splitAsList(x, f, drop=drop)
+  .Defunct("splitAsList")
 }
 
 seqby <- function(data, INDICES, FUN, ...) {
-  .Deprecated("as(by(X, INDICES, FUN, ...), 'List')")
-  .asList(by(data, INDICES, FUN, ..., simplify = FALSE))
+  .Defunct("as(by(X, INDICES, FUN, ...), 'List')")
 }
 
 ### These are excluded from the deprecation
