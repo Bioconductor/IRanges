1500a1c4495c1f2b616abe97fd98fa1f6884d136
Herve Pages <hpages@fhcrc.org>
2014-11-14 10:36:04 +0000

    start unit tests for NCList objects and findOverlaps_NCList() (non-exported)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@96757 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_NCList-class.R
@@ -0,0 +1,84 @@
+###
+
+findOverlaps_NCList <- IRanges:::findOverlaps_NCList
+
+.findOverlaps_naive <- function(query, subject,
+                           type=c("any", "start", "end", "within", "equal"),
+                           select=c("all", "first", "last"))
+{
+    type <- match.arg(type)
+    select <- match.arg(select)
+    type_codes <- switch(type,
+        "any"=c("c", "d", "e", "f", "g", "h", "i", "j", "k"),
+        "start"=c("f", "g", "h"),
+        "end"=c("d", "g", "j"),
+        "within"=c("f", "g", "i", "j"),
+        "equal"="g"
+    )
+    hits_per_query <- lapply(seq_along(query),
+        function(i) {
+            codes <- rangeComparisonCodeToLetter(compare(query[i], subject))
+            which(codes %in% type_codes)
+        })
+    if (select == "all") {
+        q_hits <- rep.int(seq_along(query), elementLengths(hits_per_query))
+        s_hits <- unlist(hits_per_query, use.names=FALSE)
+        ans <- new("Hits", queryHits=q_hits, subjectHits=s_hits,
+                           queryLength=length(query),
+                           subjectLength=length(subject))
+        return(ans)
+    }
+    ans <- integer(length(query))
+    ans[] <- NA_integer_
+    idx1 <- which(elementLengths(hits_per_query) != 0L)
+    select_FUN <- switch(select, "first"=min, "last"=max)
+    ans[idx1] <- sapply(hits_per_query[idx1], select_FUN)
+    ans
+}
+
+.compareHits <- function(target, current) identical(t(target), t(current))
+
+test_findOverlaps_NCList <- function()
+{
+    query <- IRanges(-3:7, width=3)
+    subject <- IRanges(rep.int(1:5, 5:1), c(1:5, 2:5, 3:5, 4:5, 5))
+
+    current0 <- findOverlaps_NCList(query, NCList(subject))
+    target0 <- .findOverlaps_naive(query, subject)
+    checkTrue(.compareHits(target0, current0))
+
+    permute_subject <- function(perm) {
+        current <- findOverlaps_NCList(query, NCList(subject[perm]))
+        revperm <- integer(length(perm))
+        revperm[perm] <- seq_along(perm)
+        target <- target0
+        target@subjectHits <- revperm[target@subjectHits]
+        checkTrue(.compareHits(target, current))
+    }
+
+    ## Reverse subject.
+    permute_subject(rev(seq_along(subject)))
+
+    ## Random permutations of subject.
+    set.seed(333)
+    for (i in 1:99)
+        permute_subject(sample(length(subject)))
+}
+
+test_findOverlaps_NCList_type_select <- function()
+{
+    query <- IRanges(-3:7, width=3)
+    subject <- IRanges(rep.int(1:5, 5:1), c(1:5, 2:5, 3:5, 4:5, 5))
+
+    pp_subject <- NCList(subject)
+    for (type in c("any", "start", "end", "within", "equal")) {
+        for (select in c("all", "first", "last")) {
+            current <- findOverlaps_NCList(query, pp_subject,
+                                           type=type, select=select)
+            target <- .findOverlaps_naive(query, subject,
+                                         type=type, select=select)
+            checkTrue(.compareHits(target, current))
+        }
+    }
+}
+
