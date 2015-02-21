fcb15c9b70842c53eb4cdd64cf4dc8a55f1a051b
Herve Pages <hpages@fhcrc.org>
2015-02-21 02:30:24 +0000

    The RleViewsList constructor now reorders 'rleList' so that its names match the names on 'rangesList'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@99682 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RleViewsList-class.R
+++ R/RleViewsList-class.R
@@ -30,27 +30,47 @@ setMethod("Views", "RleList",
 RleViewsList <- function(..., rleList, rangesList, universe = NULL)
 {
     if (!is.null(universe) && !isSingleString(universe))
-        stop("'universe' must be a single string or NULL")
+        stop(wmsg("'universe' must be a single string or NULL"))
     views <- list(...)
     if (!missing(rleList) && !missing(rangesList)) {
         if (length(views) > 0)
-            stop("'...' must be empty when 'rleList' and 'rangesList' are specified")
+            stop(wmsg("'...' must be empty when 'rleList' and 'rangesList' ",
+                      "are specified"))
         if (!is(rleList, "RleList"))
-            stop("'rleList' must be a RleList object")
+            stop(wmsg("'rleList' must be a RleList object"))
         if (!is(rangesList, "RangesList")) {
             rangesList <- try(IRangesList(rangesList), silent = TRUE)
             if (inherits(rangesList, "try-error"))
-                stop("'rangesList' must be a RangesList object")
+                stop(wmsg("'rangesList' must be a RangesList object"))
+        }
+        if (length(rleList) != length(rangesList))
+            stop("'rleList' and 'rangesList' must have the same length")
+        rleList_names <- names(rleList)
+        rangesList_names <- names(rangesList)
+        if (!(is.null(rleList_names) ||
+              is.null(rangesList_names) ||
+              identical(rleList_names, rangesList_names))) {
+            if (anyDuplicated(rleList_names,) ||
+                anyDuplicated(rangesList_names))
+                stop(wmsg("when both 'rleList' and 'rangesList' have names, ",
+                          "the names on each object cannot have duplicates"))
+            if (!setequal(rleList_names, rangesList_names))
+                stop(wmsg("when both 'rleList' and 'rangesList' have names, ",
+                          "the set of names must be the same on each object"))
+            warning(wmsg("'rleList' was reordered so that its names ",
+                         "match the names on 'rangesList'"))
+            rleList <- rleList[rangesList_names]
         }
         views <- Map(Views, rleList, rangesList)
     } else if ((length(views) > 0) &&
             (!missing(rleList) || !missing(rangesList))) {
-        stop("cannot specify 'rleList' or 'rangesList' when specifying '...'")
+        stop(wmsg("cannot specify 'rleList' or 'rangesList' ",
+                  "when specifying '...'"))
     } else {
         if (length(views) == 1 && is.list(views[[1L]]))
             views <- views[[1L]]
         if (!all(sapply(views, is, "RleViews")))
-            stop("all elements in '...' must be RleViews objects")
+            stop(wmsg("all elements in '...' must be RleViews objects"))
     }
     ans <- S4Vectors:::new_SimpleList_from_list("SimpleRleViewsList", views)
     universe(ans) <- universe
