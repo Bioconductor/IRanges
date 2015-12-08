6b5d24ba0a40a787927b209033db2c93f1c256c5
Michael Lawrence <lawremi@iastate.edu>
2015-12-08 14:27:21 +0000

    starting on optimized prod() and is.unsorted() for compressed lists
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@111300 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/CompressedAtomicList_utils.c
+++ src/CompressedAtomicList_utils.c
@@ -11,6 +11,11 @@
                     summary += val);                                          \
 }
 
+#define PARTITIONED_PROD(C_TYPE, ACCESSOR, ANS_TYPE, ANS_ACCESSOR, NA_CHECK) { \
+    PARTITIONED_AGG(C_TYPE, ACCESSOR, ANS_TYPE, ANS_ACCESSOR, NA_CHECK, 1,     \
+                    summary *= val);					       \
+}
+
 #define PARTITIONED_MIN(C_TYPE, ACCESSOR, ANS_TYPE, NA_CHECK, INIT) {         \
     PARTITIONED_AGG(C_TYPE, ACCESSOR, ANS_TYPE, ACCESSOR, NA_CHECK, INIT,     \
                     if (val < summary) summary = val);                        \
@@ -31,7 +36,7 @@
     SEXP ans = allocVector(ANS_TYPE, length(ends));                     \
     for (int i = 0; i < length(ends); i++) {                            \
       int end = INTEGER(ends)[i];                                       \
-      int summary = INIT;                                               \
+      C_TYPE summary = INIT;                                               \
       for (int j = prev_end; j < end; j++) {                            \
         C_TYPE val = ACCESSOR(unlistData)[j];                           \
         if (NA_CHECK) {                                                 \
@@ -74,6 +79,30 @@ SEXP CompressedIntegerList_sum(SEXP x, SEXP na_rm)
 SEXP CompressedNumericList_sum(SEXP x, SEXP na_rm)
 {
   PARTITIONED_SUM(double, REAL, REALSXP, REAL, ISNA(val));
+}
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedLogicalList_prod(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_PROD(Rboolean, LOGICAL, INTSXP, INTEGER, val == NA_LOGICAL);
+}
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedIntegerList_prod(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_PROD(int, INTEGER, INTSXP, INTEGER, val == NA_INTEGER);
+}
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedNumericList_prod(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_PROD(double, REAL, REALSXP, REAL, ISNA(val));
 }
 
 /*
@@ -122,4 +151,65 @@ SEXP CompressedIntegerList_max(SEXP x, SEXP na_rm)
 SEXP CompressedNumericList_max(SEXP x, SEXP na_rm)
 {
   PARTITIONED_MAX(double, REAL, REALSXP, ISNA(val), R_NegInf);
+}
+
+#define PARTITIONED_IS_UNSORTED(C_TYPE, ACCESSOR, NA_CHECK) {		\
+    PARTITIONED_BREAK(C_TYPE, ACCESSOR, NA_CHECK,			\
+		      j > 0 && val < ACCESSOR(unlistData)[j-1]);	\
+}
+
+#define PARTITIONED_BREAK(C_TYPE, ACCESSOR, NA_CHECK, BREAK_CHECK) {	\
+    SEXP unlistData = _get_CompressedList_unlistData(x);                \
+    SEXP ends =                                                         \
+      _get_PartitioningByEnd_end(_get_CompressedList_partitioning(x));  \
+    Rboolean _na_rm = asLogical(na_rm);                                 \
+    int prev_end = 0;                                                   \
+    SEXP ans = allocVector(LGLSXP, length(ends));			\
+    for (int i = 0; i < length(ends); i++) {                            \
+      int end = INTEGER(ends)[i];                                       \
+      Rboolean summary = FALSE;						\
+      for (int j = prev_end; j < end; j++) {                            \
+        C_TYPE val = ACCESSOR(unlistData)[j];                           \
+        if (NA_CHECK) {                                                 \
+          if (_na_rm) {                                                 \
+            continue;                                                   \
+          } else {                                                      \
+            summary = NA_LOGICAL;					\
+            break;                                                      \
+          }								\
+        }								\
+	if (BREAK_CHECK) {						\
+	  summary = TRUE;						\
+	  break;							\
+	}								\
+      }                                                                 \
+      LOGICAL(ans)[i] = summary;					\
+      prev_end = end;                                                   \
+    }                                                                   \
+    setAttrib(ans, R_NamesSymbol, _get_CompressedList_names(x));        \
+    return ans;                                                         \
+  }
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedLogicalList_is_unsorted(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_IS_UNSORTED(Rboolean, LOGICAL, val == NA_LOGICAL);
+}
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedIntegerList_is_unsorted(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_IS_UNSORTED(int, INTEGER, val == NA_INTEGER);
+}
+
+/*
+ * --- .Call ENTRY POINT ---
+ */
+SEXP CompressedNumericList_is_unsorted(SEXP x, SEXP na_rm)
+{
+  PARTITIONED_IS_UNSORTED(double, REAL, ISNA(val));
 }
