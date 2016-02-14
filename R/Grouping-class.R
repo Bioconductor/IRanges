a27e4c7288e0b74d5eeaf8574e3aff8224759b29
Michael Lawrence <lawremi@iastate.edu>
2016-02-14 18:10:18 +0000

    Add Simple/Compressed(ManyToOne)Grouping classes that are implemented with an actual IntegerList. Provides an integration point with 'grouping' class in R 3.3.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@113624 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Grouping-class.R
+++ R/Grouping-class.R
@@ -33,6 +33,9 @@ setClass("Grouping", contains="IntegerList", representation("VIRTUAL"))
 
 setGeneric("nobj", function(x) standardGeneric("nobj"))
 
+setMethod("nobj", "Grouping",
+          function(x) length(unique(unlist(x, use.names=FALSE))))
+
 setGeneric("grouplength", signature="x",
     function(x, i=NULL) standardGeneric("grouplength")
 )
@@ -41,8 +44,8 @@ setMethod("grouplength", "Grouping",
     function(x, i=NULL)
     {
         if (is.null(i))
-            i <- seq_len(length(x))
-        sapply(i, function(ii) length(x[[i]]))
+            lengths(x)
+        else vapply(i, function(ii) length(x[[i]]), integer(1L))
     }
 )
 
@@ -71,6 +74,8 @@ setMethod("show", "Grouping",
 ### A ManyToOneGrouping object represents a grouping where every object in
 ### the collection is mapped to one group and only one.
 setClass("ManyToOneGrouping", contains="Grouping", representation("VIRTUAL"))
+
+setMethod("nobj", "ManyToOneGrouping", function(x) sum(grouplength(x)))
 
 setGeneric("members", signature="x",
     function(x, i) standardGeneric("members")
@@ -971,3 +976,100 @@ findOverlaps_Ranges_Partitioning <- function(query, subject,
     Hits(q_hits, s_hits, q_len, s_len)
 }   
 
+### -------------------------------------------------------------------------
+### Simple(ManyToOne)Grouping and Compressed(ManyToOne)Grouping
+### ----------------------------
+###
+
+setClass("SimpleGrouping",
+         contains=c("Grouping", "SimpleIntegerList"))
+
+setClass("CompressedGrouping",
+         contains=c("Grouping", "CompressedIntegerList"))
+
+setClass("SimpleManyToOneGrouping",
+         contains=c("ManyToOneGrouping", "SimpleGrouping"))
+
+setClass("CompressedManyToOneGrouping",
+         contains=c("ManyToOneGrouping", "CompressedGrouping"))
+
+### -------------------------------------------------------------------------
+### Grouping API implementation
+### ----------------------------
+###
+
+setMethod("grouplength", "CompressedGrouping",
+          function(x, i=NULL) grouplength(PartitioningByEnd(x), i))
+
+setMethod("nobj", "CompressedManyToOneGrouping",
+          function(x) nobj(PartitioningByEnd(x)))
+
+setMethods("togroup",
+           list("SimpleManyToOneGrouping", "CompressedManyToOneGrouping"),
+           function(x, j=NULL) {
+               g <- callNextMethod(x)
+               if (!is.null(j)) {
+                   x <- x[j]
+               }
+               ans <- unlist(x, use.names=FALSE)
+               ans[ans] <- g
+               ans
+           })
+
+### -------------------------------------------------------------------------
+### Constructors
+### ----------------------------
+###
+
+SimpleGrouping <- function(x) {
+    new("SimpleGrouping", IntegerList(x, compress=FALSE))
+}
+
+CompressedGrouping <- function(x) {
+    new("CompressedGrouping", IntegerList(x, compress=TRUE))
+}
+
+SimpleManyToOneGrouping <- function(x) {
+    new("SimpleManyToOneGrouping", IntegerList(x, compress=FALSE))
+}
+
+CompressedManyToOneGrouping <- function(x) {
+    new("CompressedManyToOneGrouping", IntegerList(x, compress=TRUE))
+}
+
+### -------------------------------------------------------------------------
+### Coercion
+### ----------------------------
+###
+
+setOldClass("grouping")
+
+## utils::relist dipatches only on 'skeleton' so this is here instead of in R
+setMethod("relist", c("grouping", "missing"), function(flesh, skeleton) {
+              relist(flesh, PartitioningByEnd(attr(flesh, "ends")))
+          })
+
+setMethod("split", c("ANY", "ManyToOneGrouping"), function(x, f, drop=FALSE) {
+              stopifnot(isTRUEorFALSE(drop))
+              if (drop) {
+                  f <- f[grouplength(f) > 0L]
+              }
+              extractList(x, f)
+          })
+
+setAs("grouping", "Grouping", function(from) {
+          CompressedGrouping(relist(from))
+      })
+
+setAs("grouping", "ManyToOneGrouping", function(from) {
+          CompressedManyToOneGrouping(relist(from))
+      })
+
+setAs("factor", "Grouping", function(from) {
+          as(grouping(from), "Grouping")
+      })
+
+setAs("ManyToOneGrouping", "factor", function(from) {
+          structure(togroup(from), levels=seq_along(from),
+                    class="factor")
+      })
