d78e7d3ddd1d37661d64193f02d20543758da0d1
Michael Lawrence <lawremi@iastate.edu>
2016-07-30 15:09:21 +0000

    let paste() coerce via as.character()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IRanges@119759 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AtomicList-impl.R
+++ R/AtomicList-impl.R
@@ -486,7 +486,7 @@ setMethod("showAsCell", "AtomicList",
               if (length(object) == 0L)
                 return(character(0))
               unlist(lapply(object, function(x) {
-                str <- paste(as.vector(head(x, 3)), collapse = ",") 
+                str <- paste(head(x, 3), collapse = ",") 
                 if (length(x) > 3)
                   str <- paste(str, "...", sep = ",")
                 str
