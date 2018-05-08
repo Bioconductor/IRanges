14b7db9761360f4d9446e2620747c7f672de23f7
Hervé Pagès <hpages@fredhutch.org>
2018-05-08 02:33:32 -0700

    Rename 2nd arg of splitListElements() -> 'splitter'

--- R/splitListElements.R
+++ R/splitListElements.R
@@ -53,49 +53,49 @@ regroupBySupergroup <- function(x, supergroups)
 ### splitListElements()
 ###
 
-INCOMPATIBLE_PARTITIONING_MSG <- c(
-    "'partitioning' is incompatible with the cumulated ",
+INCOMPATIBLE_SPLITTER_MSG <- c(
+    "'splitter' is incompatible with the cumulated ",
     "length of all the list elements in 'x'"
 )
 
 ### Return a Partitioning object of the same class as 'x' (endomorphism)
 ### if 'hits.only' is FALSE (the default). Otherwise, return a list of 2
 ### integer vectors of the same length.
-.split_partitions <- function(x, partitioning, hits.only=FALSE,
-                              msg.if.incompatible=INCOMPATIBLE_PARTITIONING_MSG)
+.split_partitions <- function(x, splitter, hits.only=FALSE,
+                              msg.if.incompatible=INCOMPATIBLE_SPLITTER_MSG)
 {
     if (!is(x, "Partitioning"))
         stop(wmsg("'x' must be a Partitioning object"))
-    if (!is(partitioning, "Partitioning"))
-        stop(wmsg("'partitioning' must be a Partitioning object"))
+    if (!is(splitter, "Partitioning"))
+        stop(wmsg("'splitter' must be a Partitioning object"))
     if (!isTRUEorFALSE(hits.only))
         stop(wmsg("'hits.only' must be TRUE or FALSE"))
     if (!is.character(msg.if.incompatible))
         stop(wmsg("'msg.if.incompatible' must be a character vector"))
     x_end <- end(x)
-    partitioning_end <- end(partitioning)
+    splitter_end <- end(splitter)
     if (S4Vectors:::last_or(x_end, 0L) !=
-        S4Vectors:::last_or(partitioning_end, 0L))
+        S4Vectors:::last_or(splitter_end, 0L))
         stop(wmsg(msg.if.incompatible))
     C_ans <- .Call2("find_partition_overlaps",
-                    x_end, partitioning_end, !hits.only, PACKAGE="IRanges")
+                    x_end, splitter_end, !hits.only, PACKAGE="IRanges")
     if (hits.only)
         return(C_ans)
     revmap <- C_ans[[1L]]
-    partition <- C_ans[[2L]]
+    revmap2 <- C_ans[[2L]]
     ans <- as(PartitioningByEnd(C_ans[[3L]], names=names(x)[revmap]), class(x))
-    mcols(ans) <- DataFrame(revmap=revmap, partition=partition)
+    mcols(ans) <- DataFrame(revmap=revmap, revmap2=revmap2)
     ans
 }
 
 ### Act as an endomorphism.
-splitListElements <- function(x, partitioning, use.mcols=FALSE,
-                              msg.if.incompatible=INCOMPATIBLE_PARTITIONING_MSG)
+splitListElements <- function(x, splitter, use.mcols=FALSE,
+                              msg.if.incompatible=INCOMPATIBLE_SPLITTER_MSG)
 {
     if (!isTRUEorFALSE(use.mcols))
         stop(wmsg("'use.mcols' must be TRUE or FALSE"))
     if (is(x, "Partitioning")) {
-        ans <- .split_partitions(x, partitioning,
+        ans <- .split_partitions(x, splitter,
                                  msg.if.incompatible=msg.if.incompatible)
         if (use.mcols)
             mcols(ans) <- mcols(x)[mcols(ans)$revmap, , drop=FALSE]
@@ -113,17 +113,17 @@ splitListElements <- function(x, partitioning, use.mcols=FALSE,
     ## DNAStringSet, DNAStringSetList, GAlignments, GAlignmentsList objects
     ## and more...
     x_partitioning <- PartitioningByEnd(x)
-    hits <- .split_partitions(x_partitioning, partitioning, hits.only=TRUE,
+    hits <- .split_partitions(x_partitioning, splitter, hits.only=TRUE,
                               msg.if.incompatible=msg.if.incompatible)
     revmap <- hits[[1L]]
-    partition <- hits[[2L]]
+    revmap2 <- hits[[2L]]
     ans <- x[revmap]
     if (!use.mcols)
-        mcols(ans) <- DataFrame(revmap=revmap, partition=partition)
-    Ltrim <- pmax(start(partitioning)[partition] -
+        mcols(ans) <- DataFrame(revmap=revmap, revmap2=revmap2)
+    Ltrim <- pmax(start(splitter)[revmap2] -
                   start(x_partitioning)[revmap], 0L)
     Rtrim <- pmax(end(x_partitioning)[revmap] -
-                  end(partitioning)[partition], 0L)
+                  end(splitter)[revmap2], 0L)
     windows(ans, start=1L+Ltrim, end=-1L-Rtrim)
 }
 
@@ -144,18 +144,18 @@ equisplit <- function(x, nchunk, chunksize, use.mcols=FALSE)
     if (!isTRUEorFALSE(use.mcols))
         stop(wmsg("'use.mcols' must be TRUE or FALSE"))
     x_eltNROWS <- elementNROWS(x)
-    partitioning <- breakInChunks(sum(x_eltNROWS),
-                                  nchunk=nchunk, chunksize=chunksize)
-    unlisted_ans <- splitListElements(x, partitioning)
+    splitter <- breakInChunks(sum(x_eltNROWS),
+                              nchunk=nchunk, chunksize=chunksize)
+    unlisted_ans <- splitListElements(x, splitter)
     unlisted_ans_mcols <- mcols(unlisted_ans)
     revmap <- unlisted_ans_mcols[ , "revmap"]
-    partition <- unlisted_ans_mcols[ , "partition"]
+    revmap2 <- unlisted_ans_mcols[ , "revmap2"]
     if (use.mcols) {
         mcols(unlisted_ans) <- mcols(x)[revmap, , drop=FALSE]
     } else {
         mcols(unlisted_ans) <- DataFrame(revmap=revmap)
     }
-    ans_partitioning <- PartitioningByEnd(partition, NG=length(partitioning))
+    ans_partitioning <- PartitioningByEnd(revmap2, NG=length(splitter))
     relist(unlisted_ans, ans_partitioning)
 }
 
