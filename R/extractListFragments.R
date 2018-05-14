fef6ea2062d20bc26b63d1a7a79d35c133b7e37b
Hervé Pagès <hpages@fredhutch.org>
2018-05-14 08:37:11 -0700

    Rename 2nd argument of extractListFragments() -> 'aranges'

--- R/extractListFragments.R
+++ R/extractListFragments.R
@@ -53,33 +53,33 @@ regroupBySupergroup <- function(x, supergroups)
 ### extractListFragments()
 ###
 
-INCOMPATIBLE_FRAGMENTER_MSG <- c(
-    "'fragmenter' is incompatible with the cumulated ",
+INCOMPATIBLE_ARANGES_MSG <- c(
+    "'aranges' is incompatible with the cumulated ",
     "length of all the list elements in 'x'"
 )
 
 ### If 'hits.only' is FALSE (the default), return a Partitioning object of
 ### the same class as 'x' (endomorphism). Otherwise, return a list of 2
 ### integer vectors of the same length.
-.extractPartitioningFragments_by_Partitioning <- function(x, fragmenter,
+.extractPartitioningFragments_by_Partitioning <- function(x, aranges,
                               hits.only=FALSE,
-                              msg.if.incompatible=INCOMPATIBLE_FRAGMENTER_MSG)
+                              msg.if.incompatible=INCOMPATIBLE_ARANGES_MSG)
 {
     if (!is(x, "Partitioning"))
         stop(wmsg("'x' must be a Partitioning object"))
-    if (!is(fragmenter, "Partitioning"))
-        stop(wmsg("'fragmenter' must be a Partitioning object"))
+    if (!is(aranges, "Partitioning"))
+        stop(wmsg("'aranges' must be a Partitioning object"))
     if (!isTRUEorFALSE(hits.only))
         stop(wmsg("'hits.only' must be TRUE or FALSE"))
     if (!is.character(msg.if.incompatible))
         stop(wmsg("'msg.if.incompatible' must be a character vector"))
     x_end <- end(x)
-    fragmenter_end <- end(fragmenter)
+    aranges_end <- end(aranges)
     if (S4Vectors:::last_or(x_end, 0L) !=
-        S4Vectors:::last_or(fragmenter_end, 0L))
+        S4Vectors:::last_or(aranges_end, 0L))
         stop(wmsg(msg.if.incompatible))
     C_ans <- .Call2("find_partition_overlaps",
-                    x_end, fragmenter_end, !hits.only, PACKAGE="IRanges")
+                    x_end, aranges_end, !hits.only, PACKAGE="IRanges")
     if (hits.only)
         return(C_ans)
     revmap <- C_ans[[1L]]
@@ -92,12 +92,12 @@ INCOMPATIBLE_FRAGMENTER_MSG <- c(
     ans
 }
 
-.extractListFragments_by_Partitioning <- function(x, fragmenter,
+.extractListFragments_by_Partitioning <- function(x, aranges,
                       use.mcols=FALSE,
-                      msg.if.incompatible=INCOMPATIBLE_FRAGMENTER_MSG)
+                      msg.if.incompatible=INCOMPATIBLE_ARANGES_MSG)
 {
     if (is(x, "Partitioning")) {
-        ans <- .extractPartitioningFragments_by_Partitioning(x, fragmenter,
+        ans <- .extractPartitioningFragments_by_Partitioning(x, aranges,
                                    msg.if.incompatible=msg.if.incompatible)
         if (use.mcols) {
             revmap <- mcols(ans)[ , "revmap"]
@@ -118,7 +118,7 @@ INCOMPATIBLE_FRAGMENTER_MSG <- c(
     ## and more...
     x_partitioning <- PartitioningByEnd(x)
     hits <- .extractPartitioningFragments_by_Partitioning(
-                                   x_partitioning, fragmenter,
+                                   x_partitioning, aranges,
                                    hits.only=TRUE,
                                    msg.if.incompatible=msg.if.incompatible)
     revmap <- hits[[1L]]
@@ -126,36 +126,36 @@ INCOMPATIBLE_FRAGMENTER_MSG <- c(
     ans <- x[revmap]
     if (!use.mcols)
         mcols(ans) <- DataFrame(revmap=revmap, revmap2=revmap2)
-    Ltrim <- pmax(start(fragmenter)[revmap2] -
+    Ltrim <- pmax(start(aranges)[revmap2] -
                   start(x_partitioning)[revmap], 0L)
     Rtrim <- pmax(end(x_partitioning)[revmap] -
-                  end(fragmenter)[revmap2], 0L)
+                  end(aranges)[revmap2], 0L)
     windows(ans, start=1L+Ltrim, end=-1L-Rtrim)
 }
 
-### Return a PartitioningByEnd object of length 2 * length(fragmenter) + 1.
-.make_PartitioningByEnd_from_fragmenter <- function(fragmenter, x,
-                                                    msg.if.incompatible)
+### Return a PartitioningByEnd object of length 2 * length(aranges) + 1.
+.make_PartitioningByEnd_from_aranges <- function(aranges, x,
+                                                 msg.if.incompatible)
 {
-    if (!is(fragmenter, "IntegerRanges"))
-        stop(wmsg("'fragmenter' must be an IntegerRanges derivative ",
+    if (!is(aranges, "IntegerRanges"))
+        stop(wmsg("'aranges' must be an IntegerRanges derivative ",
                   "(e.g. an IRanges object"))
 
-    ## Check that 'fragmenter' is disjoint and sorted.
-    ## This is the case if and only if 'start_end' is sorted. If 'fragmenter'
+    ## Check that 'aranges' is disjoint and sorted.
+    ## This is the case if and only if 'start_end' is sorted. If 'aranges'
     ## is a NormalIRanges or Partitioning object, then it's disjoint and sorted
     ## so we can skip this check.
     start_end <- as.vector(
-        rbind(start(fragmenter) - 1L, end(fragmenter), deparse.level=0)
+        rbind(start(aranges) - 1L, end(aranges), deparse.level=0)
     )
-    if (!is(fragmenter, "NormalIRanges") &&
-        !is(fragmenter, "Partitioning") &&
+    if (!is(aranges, "NormalIRanges") &&
+        !is(aranges, "Partitioning") &&
         S4Vectors:::isNotSorted(start_end))
-        stop(wmsg("'fragmenter' must be disjoint and sorted"))
+        stop(wmsg("'aranges' must be disjoint and sorted"))
 
-    ## Check that 'fragmenter' is compatible with 'x'.
+    ## Check that 'aranges' is compatible with 'x'.
     x_cumlen <- nobj(PartitioningByEnd(x))
-    start_end_len <- length(start_end)  # = 2 * length(fragmenter)
+    start_end_len <- length(start_end)  # = 2 * length(aranges)
     if (start_end_len >= 2L &&
         (start_end[[1L]] < 0L || start_end[[start_end_len]] > x_cumlen))
         stop(wmsg(msg.if.incompatible))
@@ -166,23 +166,23 @@ INCOMPATIBLE_FRAGMENTER_MSG <- c(
 
 ### Act as an endomorphism.
 ### 'x' must be a list-like object.
-### 'fragmenter' must be an IntegerRanges object that is disjoint, sorted,
+### 'aranges' must be an IntegerRanges object that is disjoint, sorted,
 ### and compatible with the cumulated length of all the list elements in 'x'.
-extractListFragments <- function(x, fragmenter,
+extractListFragments <- function(x, aranges,
                             use.mcols=FALSE,
-                            msg.if.incompatible=INCOMPATIBLE_FRAGMENTER_MSG)
+                            msg.if.incompatible=INCOMPATIBLE_ARANGES_MSG)
 {
     if (!isTRUEorFALSE(use.mcols))
         stop(wmsg("'use.mcols' must be TRUE or FALSE"))
-    if (is(fragmenter, "Partitioning")) {
-        ans <- .extractListFragments_by_Partitioning(x, fragmenter,
+    if (is(aranges, "Partitioning")) {
+        ans <- .extractListFragments_by_Partitioning(x, aranges,
                                      use.mcols=use.mcols,
                                      msg.if.incompatible=msg.if.incompatible)
         return(ans)
     }
-    fragmenter <- .make_PartitioningByEnd_from_fragmenter(fragmenter, x,
-                                               INCOMPATIBLE_FRAGMENTER_MSG)
-    ans <- .extractListFragments_by_Partitioning(x, fragmenter,
+    aranges <- .make_PartitioningByEnd_from_aranges(aranges, x,
+                                               INCOMPATIBLE_ARANGES_MSG)
+    ans <- .extractListFragments_by_Partitioning(x, aranges,
                                  msg.if.incompatible=msg.if.incompatible)
     revmap2 <- mcols(ans)[ , "revmap2"]
     ans <- ans[revmap2 %% 2L == 0L]
@@ -212,8 +212,8 @@ equisplit <- function(x, nchunk, chunksize, use.mcols=FALSE)
     if (!isTRUEorFALSE(use.mcols))
         stop(wmsg("'use.mcols' must be TRUE or FALSE"))
     x_cumlen <- nobj(PartitioningByEnd(x))
-    fragmenter <- breakInChunks(x_cumlen, nchunk=nchunk, chunksize=chunksize)
-    unlisted_ans <- extractListFragments(x, fragmenter)
+    aranges <- breakInChunks(x_cumlen, nchunk=nchunk, chunksize=chunksize)
+    unlisted_ans <- extractListFragments(x, aranges)
     unlisted_ans_mcols <- mcols(unlisted_ans)
     revmap <- unlisted_ans_mcols[ , "revmap"]
     revmap2 <- unlisted_ans_mcols[ , "revmap2"]
@@ -222,7 +222,7 @@ equisplit <- function(x, nchunk, chunksize, use.mcols=FALSE)
     } else {
         mcols(unlisted_ans) <- DataFrame(revmap=revmap)
     }
-    ans_partitioning <- PartitioningByEnd(revmap2, NG=length(fragmenter))
+    ans_partitioning <- PartitioningByEnd(revmap2, NG=length(aranges))
     relist(unlisted_ans, ans_partitioning)
 }
 
