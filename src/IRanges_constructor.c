0814306aa4159f27444394b1e95d105a8e6294a6
Hervé Pagès <hpages@fredhutch.org>
2019-08-22 16:55:26 -0700

    Rename (and improve) C-level helper solve_user_SEW0_row() -> solve_range()

--- src/IRanges_constructor.c
+++ src/IRanges_constructor.c
@@ -3,6 +3,8 @@
  ****************************************************************************/
 #include "IRanges.h"
 
+#define	R_INT_MIN (INT_MIN + 1)
+#define	R_INT_MAX INT_MAX
 
 static char errmsg_buf[200];
 
@@ -11,33 +13,91 @@ static char errmsg_buf[200];
  * solve_user_SEW0()
  */
 
-static int solve_user_SEW0_row(int start, int end, int width,
+static int solve_range(int start, int end, int width,
 		int *solved_start, int *solved_width)
 {
-	int nb_of_unknowns;
+	long long int tmp;
+	static const char *too_many_NAs = "at least two out of 'start', "
+					  "'end', and 'width', must\n  be "
+					  "supplied";
 
-	nb_of_unknowns = (start == NA_INTEGER) + (end == NA_INTEGER) + (width == NA_INTEGER);
-	if (nb_of_unknowns >= 2) {
-		snprintf(errmsg_buf, sizeof(errmsg_buf),
-			 "range cannot be determined from the supplied arguments (too many NAs)");
-		return -1;
-	}
-	if (start == NA_INTEGER) {
-		start = end - width + 1;
-	} else if (width == NA_INTEGER) {
-		width = end - start + 1;
-	} else if (end != NA_INTEGER && end != start + width - 1) {
-		snprintf(errmsg_buf, sizeof(errmsg_buf),
-			 "supplied arguments are incompatible");
-		return -1;
+	*solved_start = start;
+	*solved_width = width;
+	if (width == NA_INTEGER) {
+		if (start == NA_INTEGER || end == NA_INTEGER) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "%s", too_many_NAs);
+			return -1;
+		}
+		/* Compute and check 'width'. */
+		tmp = (long long int) end - start + 1;
+		if (tmp < 0) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "'end' must be >= 'start' - 1");
+			return -1;
+		}
+		if (tmp > R_INT_MAX) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "the 'width' (%lld) inferred from the "
+				 "supplied 'start'\n  and 'end' is too big "
+				 "(>= 2^31)", tmp);
+			return -1;
+		}
+		*solved_width = (int) tmp;
+		return 0;
 	}
 	if (width < 0) {
 		snprintf(errmsg_buf, sizeof(errmsg_buf),
 			 "negative widths are not allowed");
 		return -1;
 	}
-	*solved_start = start;
-	*solved_width = width;
+	if (start == NA_INTEGER) {
+		if (end == NA_INTEGER) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "%s", too_many_NAs);
+			return -1;
+		}
+		/* Compute and check 'start'. */
+		tmp = (long long int) end - width + 1;
+		if (tmp < R_INT_MIN || tmp > R_INT_MAX) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "the 'start' (%lld) inferred from the "
+				 "supplied 'end'\n  and 'width' is beyond "
+				 "the limits of what is currently supported "
+				 "(must\n  be > -2^31 and < 2^31 for now)",
+				 tmp);
+			return -1;
+		}
+		*solved_start = (int) tmp;
+		return 0;
+	}
+	if (end == NA_INTEGER) {
+		if (start == NA_INTEGER) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "%s", too_many_NAs);
+			return -1;
+		}
+		/* Compute and check 'end'. */
+		tmp = (long long int) start + width - 1;
+		if (tmp < R_INT_MIN || tmp > R_INT_MAX) {
+			snprintf(errmsg_buf, sizeof(errmsg_buf),
+				 "the 'end' (%lld) inferred from the "
+				 "supplied 'start'\n  and 'width' is beyond "
+				 "the limits of what is currently supported "
+				 "(must\n  be > -2^31 and < 2^31 for now)",
+				 tmp);
+			return -1;
+		}
+		return 0;
+	}
+	tmp = (long long int) end - start + 1;
+	if (width != tmp) {
+		snprintf(errmsg_buf, sizeof(errmsg_buf),
+			 "the supplied 'width' (%d) doesn't match "
+			 "the width\n  inferred from the supplied "
+			 "'start' and 'end' (%lld)", width, tmp);
+		return -1;
+	}
 	return 0;
 }
 
@@ -50,14 +110,14 @@ SEXP solve_user_SEW0(SEXP start, SEXP end, SEXP width)
 	PROTECT(ans_start = NEW_INTEGER(ans_length));
 	PROTECT(ans_width = NEW_INTEGER(ans_length));
 	for (i = 0; i < ans_length; i++) {
-		if (solve_user_SEW0_row(INTEGER(start)[i],
-					INTEGER(end)[i],
-					INTEGER(width)[i],
-					INTEGER(ans_start) + i,
-					INTEGER(ans_width) + i) != 0)
+		if (solve_range(INTEGER(start)[i],
+				INTEGER(end)[i],
+				INTEGER(width)[i],
+				INTEGER(ans_start) + i,
+				INTEGER(ans_width) + i) != 0)
 		{
 			UNPROTECT(2);
-			error("solving row %d: %s", i + 1, errmsg_buf);
+			error("In range %d: %s.", i + 1, errmsg_buf);
 		}
 	}
 	PROTECT(ans = _new_IRanges("IRanges", ans_start, ans_width, R_NilValue));
