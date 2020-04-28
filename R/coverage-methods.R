fec1c7a9dcdd7e07b77c22baa226f13aa329fc9a
Hervé Pagès <hpages@fredhutch.org>
2020-04-28 06:44:48 -0700

    Remove RangedData class

--- R/coverage-methods.R
+++ R/coverage-methods.R
@@ -97,15 +97,10 @@
 ## Some packages like easyRNASeq or TEQC pass 'width' as a named list-like
 ## object where each list element is a single number, an NA, or a NULL, when
 ## calling coverage() on an IntegerRangesList object. They do so because, for
-## whatever reason, we've been supporting this for a while, and also because,
-## in the case of the (now defunct) method for RangedData objects, the arg
-## default for 'width' used to be such a list (a named list of NULLs in that
-## case). However, it never really made sense to support a named list-like
-## object for 'width', and it makes even less sense now that the signature of
-## the method for RangedData objects has been modified (as of BioC 2.13) to use
-## the same arg defaults as the coverage() generic and all other methods.
-## TODO: Deprecate support for this. Preferred 'width' form: NULL or an integer
-## vector. An that's it.
+## whatever reason, we've been supporting this for a while. However, it never
+## really made sense to support a named list-like object for 'width'.
+## TODO: Deprecate support for this. Preferred 'width' form: NULL or an
+## integer vector. An that's it.
 .unlist_width <- function(width, x_names, x_names.label)
 {
     if (!identical(names(width), x_names))
